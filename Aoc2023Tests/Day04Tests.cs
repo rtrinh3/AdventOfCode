9f674eedefd3445996a4f40d9d00324a42e97099
Raymond Trinh <rtrinh3@gmail.com>
2023-12-04 08:10:11 -0500

    2023 day 4 tests

---
+++ Aoc2023Tests/Day04Tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aoc2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2023.Tests
+{
+    [TestClass()]
+    public class Day04Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day04(File.ReadAllText("day04-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(13, answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day04(File.ReadAllText("day04-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(32609, answer);
+        }
+
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var instance = new Day04(File.ReadAllText("day04-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(30, answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day04(File.ReadAllText("day04-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(14624680, answer);
+        }
+    }
+}
