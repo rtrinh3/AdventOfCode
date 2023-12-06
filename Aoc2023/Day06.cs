730d57b57a77df947b411e929e37138c15708239
Raymond Trinh <rtrinh3@gmail.com>
2023-12-06 07:54:26 -0500

    2023 day 6

---
+++ Aoc2023/Day06.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Aoc2023
+{
+    // https://adventofcode.com/2023/day/6
+    public class Day06
+    {
+        private string[] times;
+        private string[] distances;
+
+        public Day06(string input)
+        {
+            foreach (string line in input.ReplaceLineEndings("\n").Split('\n', StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (line.StartsWith("Time"))
+                {
+                    times = Regex.Matches(line, @"\d+").Select(m => m.Value).ToArray();
+                }
+                else if (line.StartsWith("Distance"))
+                {
+                    distances = Regex.Matches(line, @"\d+").Select(m => m.Value).ToArray();
+                }
+                else
+                {
+                    throw new Exception("What is this line");
+                }
+            }
+        }
+
+        public long Part1()
+        {
+            return Enumerable.Range(0, times.Length)
+                .Select(i => SolveRace(double.Parse(times[i]), double.Parse(distances[i])))
+                .Aggregate(1L, (a, b) => a * b);
+        }
+
+        public long Part2()
+        {
+            return SolveRace(double.Parse(string.Join("", times)), double.Parse(string.Join("", distances)));
+        }
+
+        private static long SolveRace(double time, double distance)
+        {
+            var discriminant = Math.Sqrt(time * time - 4 * distance);
+            var low = Math.Floor((time - discriminant) / 2) + 1;
+            var high = Math.Ceiling((time + discriminant) / 2) - 1;
+            return (long)(high - low + 1);
+        }
+    }
+}
---
+++ Aoc2023Tests/Day06Tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aoc2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2023.Tests
+{
+    [TestClass()]
+    public class Day06Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(288, answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(449820, answer);
+        }
+
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(71503, answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(42250895, answer);
+        }
+    }
+}
