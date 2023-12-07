19138ab0019e30c7db0d56f6fba2481e2705c1b4
Raymond Trinh <rtrinh3@gmail.com>
2023-12-07 10:07:28 -0500

    Format day 7 and optimize EvaluateHandWithJokers

--- Aoc2023/Day07.cs
+++ Aoc2023/Day07.cs
@@ -14,19 +14,22 @@ namespace Aoc2023
 
         public Day07(string input)
         {
-            hands = input.ReplaceLineEndings("\n").Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(line => {
+            hands = input.ReplaceLineEndings("\n").Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(line =>
+            {
                 var parts = line.Split(' ');
                 return (parts[0], int.Parse(parts[1]));
             }).ToArray();
         }
 
-        private static int CompareHandsPart1(string a, string b) {
+        private static int CompareHandsPart1(string a, string b)
+        {
             Debug.Assert(a.Length == b.Length);
             // Weaker < Stronger
             var handComparison = EvaluateHand(a).CompareTo(EvaluateHand(b));
             if (handComparison != 0) return handComparison;
             const string CARD_ORDER = "23456789TJQKA";
-            for (int i = 0; i < a.Length; i++) {
+            for (int i = 0; i < a.Length; i++)
+            {
                 int cardComparison = CARD_ORDER.IndexOf(a[i]).CompareTo(CARD_ORDER.IndexOf(b[i]));
                 if (cardComparison != 0) return cardComparison;
             }
@@ -34,21 +37,35 @@ namespace Aoc2023
             return 0;
         }
 
-        private static int EvaluateHand(string hand) {
+        private static int EvaluateHand(string hand)
+        {
             var groups = hand.GroupBy(c => c).Select(g => (g.Key, g.Count())).OrderByDescending(g => g.Item2).ToArray();
-            if (groups[0].Item2 >= 5) {
+            if (groups[0].Item2 >= 5)
+            {
                 return 6;
-            } else if (groups[0].Item2 == 4) {
+            }
+            else if (groups[0].Item2 == 4)
+            {
                 return 5;
-            } else if (groups[0].Item2 == 3 && groups[1].Item2 == 2) {
+            }
+            else if (groups[0].Item2 == 3 && groups[1].Item2 == 2)
+            {
                 return 4;
-            } else if (groups[0].Item2 == 3 && groups[1].Item2 == 1) {
+            }
+            else if (groups[0].Item2 == 3 && groups[1].Item2 == 1)
+            {
                 return 3;
-            } else if (groups[0].Item2 == 2 && groups[1].Item2 == 2) {
+            }
+            else if (groups[0].Item2 == 2 && groups[1].Item2 == 2)
+            {
                 return 2;
-            } else if (groups[0].Item2 == 2 && groups[1].Item2 == 1) {
+            }
+            else if (groups[0].Item2 == 2 && groups[1].Item2 == 1)
+            {
                 return 1;
-            } else {
+            }
+            else
+            {
                 return 0;
             }
         }
@@ -58,39 +75,47 @@ namespace Aoc2023
             var partOneHands = hands.ToArray();
             Array.Sort(partOneHands, (a, b) => CompareHandsPart1(a.hand, b.hand));
             int winnings = 0;
-            for (int i = 0; i < partOneHands.Length; i++) {
-                // Console.WriteLine($"Rank {i + 1}\tBid {partOneHands[i].bid}\tHand {partOneHands[i].hand}\tType {EvaluateHand(partOneHands[i].hand)}");
+            for (int i = 0; i < partOneHands.Length; i++)
+            {
                 winnings += (i + 1) * partOneHands[i].bid;
             }
             return winnings;
         }
 
-        private static IEnumerable<string> GenerateJokerSubstitutions(string hand) {
-            if (hand == "JJJJJ") return [hand];
-            var seed = hand.Where(c => c != 'J').ToList();
-            var candidates = seed.Distinct().ToList();
-            return GenerateJokerSubstitutionsImpl(seed, candidates).Select(h => string.Join("", h));
-        }
-
-        private static IEnumerable<IEnumerable<char>> GenerateJokerSubstitutionsImpl(IEnumerable<char> partialHand, IEnumerable<char> candidates) {
-            if (partialHand.Count() == 5) {
-                return [partialHand];
-            } else  {
-                return candidates.SelectMany(c => GenerateJokerSubstitutionsImpl(partialHand.Append(c), candidates));
+        private static int EvaluateHandWithJokers(string hand)
+        {
+            if (hand == "JJJJJ")
+            {
+                return EvaluateHand(hand);
+            }
+            else
+            {
+                // Replacing the jokers with the most common non-joker generates the best hand:
+                // 4 jokers: the hand becomes a five-of-a-kind
+                // 3 jokers: the hand becomes either a four-of-a-kind or a five-of-a-kind
+                // 2 jokers:
+                // - if the other cards are ABC, this creates a three-of-a-kind
+                // - if the other cards are AAB, replacing with A creates a four-of-a-kind; this is better than replacing with B which creates a full-house
+                // 1 joker:
+                // - if the other cards are ABCD, this creates one-pair, which is better than a high-card
+                // - if the other cards are AABC, this creates a three-of-a-kind, which is better than a two-pair
+                // - if the other cards are AABB, this creates a full-house
+                // - if the other cards are AAAB, replacing with A creates a four-of-a-kind; this is better than replacing with B which creates a full-house
+                char mostCommonNonJoker = hand.Where(c => c != 'J').GroupBy(c => c).MaxBy(g => g.Count()).Key;
+                string handWithSubstitutions = hand.Replace('J', mostCommonNonJoker);
+                return EvaluateHand(handWithSubstitutions);
             }
         }
 
-        private static int EvaluateHandWithJokers(string hand) {
-            return GenerateJokerSubstitutions(hand).Max(h => EvaluateHand(h));
-        }
-
-        private static int CompareHandsPart2(string a, string b) {
+        private static int CompareHandsWithJokers(string a, string b)
+        {
             Debug.Assert(a.Length == b.Length);
             // Weaker < Stronger
             var handComparison = EvaluateHandWithJokers(a).CompareTo(EvaluateHandWithJokers(b));
             if (handComparison != 0) return handComparison;
             const string CARD_ORDER = "J23456789TQKA";
-            for (int i = 0; i < a.Length; i++) {
+            for (int i = 0; i < a.Length; i++)
+            {
                 int cardComparison = CARD_ORDER.IndexOf(a[i]).CompareTo(CARD_ORDER.IndexOf(b[i]));
                 if (cardComparison != 0) return cardComparison;
             }
@@ -101,10 +126,10 @@ namespace Aoc2023
         public int Part2()
         {
             var partTwoHands = hands.ToArray();
-            Array.Sort(partTwoHands, (a, b) => CompareHandsPart2(a.hand, b.hand));
+            Array.Sort(partTwoHands, (a, b) => CompareHandsWithJokers(a.hand, b.hand));
             int winnings = 0;
-            for (int i = 0; i < partTwoHands.Length; i++) {
-                Console.WriteLine($"Rank {i + 1}\tBid {partTwoHands[i].bid}\tHand {partTwoHands[i].hand}\tType {EvaluateHandWithJokers(partTwoHands[i].hand)}");
+            for (int i = 0; i < partTwoHands.Length; i++)
+            {
                 winnings += (i + 1) * partTwoHands[i].bid;
             }
             return winnings;
