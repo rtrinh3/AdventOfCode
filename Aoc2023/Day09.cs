fb82ad65e646ffb7007408e49ccae820dcfb944d
Raymond Trinh <rtrinh3@gmail.com>
2023-12-09 07:40:00 -0500

    2023 day 9

---
+++ Aoc2023/Day09.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2023
+{
+    // https://adventofcode.com/2023/day/9
+    public class Day09(string input)
+    {
+        private int[][] histories = input.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray())
+                .ToArray();
+
+        public int Part1()
+        {
+            var answer = histories.Sum(h => Extrapolate(h));
+            return answer;
+        }
+
+        private static int Extrapolate(int[] list)
+        {
+            if (list.Length == 0) return 0;
+            if (list.Length == 1) return list[0];
+            if (list.All(x => x == 0)) return 0;
+
+            int[] differences = new int[list.Length - 1];
+            for (int i = 0; i < differences.Length; i++)
+            {
+                differences[i] = list[i + 1] - list[i];
+            }
+            var nextDifference = Extrapolate(differences);
+            return list.Last() + nextDifference;
+        }
+
+        public int Part2()
+        {
+            var answer = histories.Sum(h => Extrapolate(h.Reverse().ToArray()));
+            return answer;
+        }
+    }
+}
---
+++ Aoc2023Tests/Day09Tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aoc2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2023.Tests
+{
+    [TestClass()]
+    public class Day09Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day09(File.ReadAllText("day09-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(114, answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day09(File.ReadAllText("day09-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(1974913025, answer);
+        }
+
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var instance = new Day09(File.ReadAllText("day09-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(2, answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day09(File.ReadAllText("day09-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(884, answer);
+        }
+    }
+}
