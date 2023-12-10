be6253da7005edc97a86bae814e88375ef45a448
Raymond Trinh <rtrinh3@gmail.com>
2023-12-10 09:42:37 -0500

    2023 day 10: more tests

--- Aoc2023Tests/Day10Tests.cs
+++ Aoc2023Tests/Day10Tests.cs
@@ -8,9 +8,24 @@ using System.Threading.Tasks;
 
 namespace Aoc2023.Tests
 {
+    // https://adventofcode.com/2023/day/10
     [TestClass()]
     public class Day10Tests
     {
+        [TestMethod()]
+        public void Part1Example1Test()
+        {
+            var instance = new Day10(File.ReadAllText("day10-example1.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(4, answer);
+        }
+        [TestMethod()]
+        public void Part1Example2Test()
+        {
+            var instance = new Day10(File.ReadAllText("day10-example2.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(8, answer);
+        }
         [TestMethod()]
         public void Part1InputTest()
         {
@@ -19,6 +34,27 @@ namespace Aoc2023.Tests
             Assert.AreEqual(6806, answer);
         }
 
+        [TestMethod()]
+        public void Part2Example3Test()
+        {
+            var instance = new Day10(File.ReadAllText("day10-example3.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(4, answer);
+        }
+        [TestMethod()]
+        public void Part2Example4Test()
+        {
+            var instance = new Day10(File.ReadAllText("day10-example4.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(8, answer);
+        }
+        [TestMethod()]
+        public void Part2Example5Test()
+        {
+            var instance = new Day10(File.ReadAllText("day10-example5.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(10, answer);
+        }
         [TestMethod()]
         public void Part2InputTest()
         {
