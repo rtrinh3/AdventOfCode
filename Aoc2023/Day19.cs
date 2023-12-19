a904cb6de702ece0cdda2ecead890e47fd767b3c
Raymond Trinh <rtrinh3@gmail.com>
2023-12-19 13:47:44 -0500

    2023-19: prepare parse for part 2

--- Aoc2023/Day19.cs
+++ Aoc2023/Day19.cs
@@ -10,64 +10,67 @@ using System.Threading.Tasks;
 namespace Aoc2023
 {
     // https://adventofcode.com/2023/day/19
-    public class Day19(string input) : IAocDay
+    public class Day19 : IAocDay
     {
-        public long Part1()
+        private record Rule(string Param, string Op, long Comparand, string Target);
+        private record Workflow(Rule[] Rules, string Fallback);
+
+        private readonly Dictionary<string, Workflow> workflows;
+        private readonly string[] parts;
+
+        public Day19(string input)
         {
             string[] paragraphs = input.ReplaceLineEndings("\n").Split("\n\n", StringSplitOptions.TrimEntries);
-            string[] workflowsString = paragraphs[0].Split('\n');
-            string[] partsString = paragraphs[1].Split('\n');
+            parts = paragraphs[1].Split('\n');
 
-            Dictionary<string, string[]> workflows = new();
+            string[] workflowsString = paragraphs[0].Split('\n');
+            workflows = new Dictionary<string, Workflow>();
             foreach (string workflow in workflowsString)
             {
                 string[] headerSplit = workflow.Replace("}", "").Split('{');
                 string name = headerSplit[0];
-                string[] rules = headerSplit[1].Split(',');
-                workflows[name] = rules;
+                string[] rulesString = headerSplit[1].Split(',');
+                Rule[] conditions = rulesString.Take(rulesString.Length - 1).Select(rule =>
+                {
+                    var parseRule = Regex.Match(rule, @"([xmas])([^\d]+)(\d+):(\w+)");
+                    Debug.Assert(parseRule.Success);
+                    return new Rule(parseRule.Groups[1].Value, parseRule.Groups[2].Value, long.Parse(parseRule.Groups[3].ValueSpan), parseRule.Groups[4].Value);
+                }).ToArray();
+                string fallback = rulesString.Last();
+                workflows[name] = new Workflow(conditions, fallback);
             }
+        }
+
+        public long Part1()
+        {
             string Evaluate(Dictionary<string, int> part, string workflowName)
             {
-                foreach (string rule in workflows[workflowName])
+                var workflow = workflows[workflowName];
+                foreach (var rule in workflow.Rules)
                 {
-                    string[] ruleParts = rule.Split(':');
-                    if (ruleParts.Length == 1)
+                    switch (rule.Op)
                     {
-                        return ruleParts[0];
-                    }
-                    else if (ruleParts.Length == 2)
-                    {
-                        //Console.WriteLine($"if({ruleParts[0]}) goto Label_{ruleParts[1]};");
-                        var parsed = Regex.Match(ruleParts[0], @"([xmas])([^\d]+)(\d+)");
-                        Debug.Assert(parsed.Success, "Failed parse");
-                        switch (parsed.Groups[2].Value)
-                        {
-                            case ">":
-                                if (part[parsed.Groups[1].Value] > int.Parse(parsed.Groups[3].Value))
-                                {
-                                    return ruleParts[1];
-                                }
-                                break;
-                            case "<":
-                                if (part[parsed.Groups[1].Value] < int.Parse(parsed.Groups[3].Value))
-                                {
-                                    return ruleParts[1];
-                                }
-                                break;
-                            default:
-                                throw new Exception("Unrecognized criterion");
-                        }
-                    }
-                    else
-                    {
-                        throw new Exception("Too many colons");
+                        case ">":
+                            if (part[rule.Param] > rule.Comparand)
+                            {
+                                return rule.Target;
+                            }
+                            break;
+                        case "<":
+                            if (part[rule.Param] < rule.Comparand)
+                            {
+                                return rule.Target;
+                            }
+                            break;
+                        default:
+                            throw new Exception("Unrecognized criterion " + rule.Op);
                     }
                 }
-                throw new Exception("No match");
+                return workflow.Fallback;
             }
 
             long sum = 0;
-            foreach (string part in partsString)
+            foreach (string part in parts)
             {
                 Dictionary<string, int> gear = new();
                 foreach (var assignments in part.Replace("{", "").Replace("}", "").Split(','))
