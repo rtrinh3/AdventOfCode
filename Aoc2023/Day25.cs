6a108cc89b5ae3faf815ac5b0348b6f27b98c0be
Raymond Trinh <rtrinh3@gmail.com>
2023-12-25 09:10:41 -0500

    Tests for 2023 day 25
    I solved day 25 by inspecting the graph via Graphviz. Let's find an algorithmic solution later.

---
+++ Aoc2023/Day25.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2023
+{
+    // https://adventofcode.com/2023/day/25
+    public class Day25(string input) : IAocDay
+    {
+        public long Part1()
+        {
+            // Stub
+            return -1;
+        }
+        public long Part2()
+        {
+            return 2023_12_25;
+        }
+    }
+}
---
+++ Aoc2023Tests/Day25Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aoc2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2023.Tests
+{
+    [TestClass()]
+    public class Day25Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day25(File.ReadAllText("day25-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(54, answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day25(File.ReadAllText("day25-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(555856, answer);
+        }
+    }
+}
