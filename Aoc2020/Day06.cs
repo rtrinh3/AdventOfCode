1928945a44f3179481d701fe5b83e40c69729d29
Raymond Trinh <rtrinh3@gmail.com>
2024-03-01 16:12:40 -0500

    2020 day 06

---
+++ Aoc2020/Day06.cs
@@ -0,0 +1,33 @@
+﻿namespace Aoc2020
+{
+    // https://adventofcode.com/2020/day/6
+    // --- Day 6: Custom Customs ---
+    public class Day06(string input): IAocDay
+    {
+        private readonly string[] groups = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+
+        public long Part1()
+        {
+            var sum = groups.Sum(g => g.Where(char.IsLetter).Distinct().Count());
+            return sum;
+        }
+
+        public long Part2()
+        {
+            int sum = 0;
+            foreach (var g in groups)
+            {
+                var persons = g.Split('\n');
+                var shortest = persons.MinBy(p => p.Length);
+                foreach (var c in shortest)
+                {
+                    if (persons.All(p => p.Contains(c)))
+                    {
+                        sum++;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Aoc2020Tests/Day06Tests.cs
@@ -0,0 +1,36 @@
+﻿namespace Aoc2020.Tests
+{
+    [TestClass()]
+    public class Day06Tests
+    {
+        [TestMethod()]
+        public void Day06_Part1_ExampleTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(11, answer);
+        }
+        [TestMethod()]
+        public void Day06_Part1_InputTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(6521, answer);
+        }
+
+        [TestMethod()]
+        public void Day06_Part2_ExampleTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(6, answer);
+        }
+        [TestMethod()]
+        public void Day06_Part2_InputTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(3305, answer);
+        }
+    }
+}
