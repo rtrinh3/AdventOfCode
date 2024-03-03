b5fc149589c58350333b1107b2607f34ad14e002
Raymond Trinh <rtrinh3@gmail.com>
2024-03-03 10:34:32 -0500

    2020 day 07

---
+++ Aoc2020/Day07.cs
@@ -0,0 +1,78 @@
+﻿using AocCommon;
+using System.Text.RegularExpressions;
+
+namespace Aoc2020
+{
+    // https://adventofcode.com/2020/day/7
+    // --- Day 7: Handy Haversacks ---
+    public class Day07(string input): IAocDay
+    {
+        private const string TARGET = "shiny gold";
+        private readonly string[] lines = input.TrimEnd().ReplaceLineEndings("\n").Split('\n');
+
+        public long Part1()
+        {
+            DefaultDict<string, List<string>> ContainedMap = new();
+            foreach (string line in lines)
+            {
+                string[] firstSplit = line.Split("contain");
+                string subjectColor = Regex.Match(firstSplit[0], @"^(.+) bag").Groups[1].Value;
+                var containedMatches = Regex.Matches(firstSplit[1], @"(\d+) ([a-z ]+) bag");
+                foreach (var m in containedMatches.AsEnumerable())
+                {
+                    var quantity = m.Groups[1].Value;
+                    var contained = m.Groups[2].Value;
+                    ContainedMap[contained].Add(subjectColor);
+                }
+            }
+
+            HashSet<string> visited = new();
+            void Visit(string color)
+            {
+                if (visited.Add(color))
+                {
+                    foreach (var next in ContainedMap[color])
+                    {
+                        Visit(next);
+                    }
+                }
+            }
+            Visit(TARGET);
+            visited.Remove(TARGET);
+            var answer = visited.Count;
+            return answer;
+        }
+
+        public long Part2()
+        {
+            DefaultDict<string, List<(long quantity, string color)>> ContainsMap = new();
+            foreach (string line in lines)
+            {
+                string[] firstSplit = line.Split("contain");
+                string subjectColor = Regex.Match(firstSplit[0], @"^(.+) bag").Groups[1].Value;
+                var containedMatches = Regex.Matches(firstSplit[1], @"(\d+) ([a-z ]+) bag");
+                foreach (var m in containedMatches.AsEnumerable())
+                {
+                    var quantity = long.Parse(m.Groups[1].ValueSpan);
+                    var contained = m.Groups[2].Value;
+                    ContainsMap[subjectColor].Add((quantity, contained));
+                }
+            }
+
+            Func<string, long> numberOfBagsInColor = _ => throw new NotImplementedException();
+            numberOfBagsInColor = Memoization.Make((string color) =>
+            {
+                long sum = 0;
+                foreach (var criterion in ContainsMap[color])
+                {
+                    sum += criterion.quantity;
+                    sum += criterion.quantity * numberOfBagsInColor(criterion.color);
+                }
+                return sum;
+            });
+
+            var answer = numberOfBagsInColor(TARGET);
+            return answer;
+        }
+    }
+}
---
+++ Aoc2020Tests/Day07Tests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aoc2020;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2020.Tests
+{
+    [TestClass()]
+    public class Day07Tests
+    {
+        [TestMethod()]
+        public void Day07_Part1_ExampleTest()
+        {
+            var instance = new Day07(File.ReadAllText("day07-example1.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(4, answer);
+        }
+        [TestMethod()]
+        public void Day07_Part1_InputTest()
+        {
+            var instance = new Day07(File.ReadAllText("day07-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(224, answer);
+        }
+
+        [TestMethod()]
+        public void Day07_Part2_Example1Test()
+        {
+            var instance = new Day07(File.ReadAllText("day07-example1.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(32, answer);
+        }
+        [TestMethod()]
+        public void Day07_Part2_Example2Test()
+        {
+            var instance = new Day07(File.ReadAllText("day07-example2.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(126, answer);
+        }
+        [TestMethod()]
+        public void Day07_Part2_InputTest()
+        {
+            var instance = new Day07(File.ReadAllText("day07-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(1488, answer);
+        }
+    }
+}
