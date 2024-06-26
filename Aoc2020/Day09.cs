82aa357d0737db8a621e17d1a63f7d59d5865c40
Raymond Trinh <rtrinh3@gmail.com>
2024-06-26 11:41:23 -0400

    2020 day 09

---
+++ Aoc2020/Day09.cs
@@ -0,0 +1,60 @@
+﻿namespace Aoc2020
+{
+    // https://adventofcode.com/2020/day/9
+    // --- Day 9: Encoding Error ---
+    public class Day09(string input) : IAocDay
+    {
+        private const int PREAMBLE_LENGTH = 25;
+
+        private readonly long[] numbers = input.TrimEnd().Split('\n').Select(long.Parse).ToArray();
+
+        public long Part1()
+        {
+            return DoPart1(PREAMBLE_LENGTH);
+        }
+
+        public long DoPart1(int preambleLength)
+        {
+            for (int i = preambleLength; i < numbers.Length; i++)
+            {
+                var current = numbers[i];
+                var window = numbers.Skip(i - preambleLength).Take(preambleLength);
+                if (window.All(candidate => !window.Contains(current - candidate)))
+                {
+                    return current;
+                }
+            }
+            throw new Exception("No answer found");
+        }
+
+        public long Part2()
+        {
+            return DoPart2(PREAMBLE_LENGTH);
+        }
+
+        public long DoPart2(int preambleLength)
+        {
+            var target = DoPart1(preambleLength);
+            // range is [begin, end]
+            for (int begin = 0; begin < numbers.Length; begin++)
+            {
+                long accumulator = numbers[begin];
+                for (int end = begin + 1; end < numbers.Length; end++)
+                {
+                    accumulator += numbers[end];
+                    if (accumulator == target)
+                    {
+                        var contiguous = numbers.Skip(begin).Take(end - begin + 1);
+                        var answer = contiguous.Min() + contiguous.Max();
+                        return answer;
+                    }
+                    if (accumulator > target)
+                    {
+                        break;
+                    }
+                }
+            }
+            throw new Exception("No answer found");
+        }
+    }
+}
---
+++ Aoc2020Tests/Day09Tests.cs
@@ -0,0 +1,40 @@
+﻿namespace Aoc2020.Tests
+{
+    // https://adventofcode.com/2020/day/9
+    // --- Day 9: Encoding Error ---
+    [TestClass()]
+    public class Day09Tests
+    {
+        private const int EXAMPLE_PREAMBLE_LENGTH = 5;
+
+        [TestMethod()]
+        public void Day09_Part1_ExampleTest()
+        {
+            var instance = new Day09(File.ReadAllText("day09-example.txt"));
+            var answer = instance.DoPart1(EXAMPLE_PREAMBLE_LENGTH);
+            Assert.AreEqual(127, answer);
+        }
+        [TestMethod()]
+        public void Day09_Part1_InputTest()
+        {
+            var instance = new Day09(File.ReadAllText("day09-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(1930745883, answer);
+        }
+
+        [TestMethod()]
+        public void Day09_Part2_ExampleTest()
+        {
+            var instance = new Day09(File.ReadAllText("day09-example.txt"));
+            var answer = instance.DoPart2(EXAMPLE_PREAMBLE_LENGTH);
+            Assert.AreEqual(62, answer);
+        }
+        [TestMethod()]
+        public void Day09_Part2_InputTest()
+        {
+            var instance = new Day09(File.ReadAllText("day09-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(268878261, answer);
+        }
+    }
+}
