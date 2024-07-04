c264b9c164408f1a2b2239cd33c6348e4c54448a
Raymond Trinh <rtrinh3@gmail.com>
2024-07-03 20:07:18 -0400

    2020 day 13

---
+++ Aoc2020/Day13.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Resources;
+
+namespace Aoc2020
+{
+    // https://adventofcode.com/2020/day/13
+    // --- Day 13: Shuttle Search ---
+    public class Day13(string input) : IAocDay
+    {
+        public long Part1()
+        {
+            var lines = input.TrimEnd().ReplaceLineEndings("\n").Split('\n');
+            long now = long.Parse(lines[0]);
+            string[] busses = lines[1].Split(',');
+
+            long earliestBus = -1;
+            long earliestPassage = -1;
+            foreach (string bus in busses)
+            {
+                if (long.TryParse(bus, out long validBus))
+                {
+                    long passage = (now / validBus) * validBus;
+                    if (passage < now)
+                    {
+                        passage += validBus;
+                    }
+                    if (earliestPassage < 0 || passage < earliestPassage)
+                    {
+                        earliestBus = validBus;
+                        earliestPassage = passage;
+                    }
+                }
+            }
+            long wait = earliestPassage - now;
+            long answer = earliestBus * wait;
+            return answer;
+        }
+
+        public long Part2()
+        {
+            var lines = input.TrimEnd().ReplaceLineEndings("\n").Split('\n');
+            // (The first line in your input is no longer relevant.)
+            string[] busses = lines[1].Split(',');
+            List<(BigInteger Factor, BigInteger Remainder)> validBusses = [];
+            for (int i = 0; i < busses.Length; i++)
+            {
+                if (BigInteger.TryParse(busses[i], out BigInteger validBus))
+                {
+                    // The bus needs to arrive at (t + i):
+                    // t + i = 0 (mod bus)
+                    // t = -i = bus - i (mod bus)
+                    validBusses.Add((validBus, (i == 0) ? 0 : validBus - i));
+                }
+            }
+
+            // https://en.wikipedia.org/wiki/Chinese_remainder_theorem#Existence_(constructive_proof)
+            var (factorA, remainderA) = validBusses[0];
+            for (int i = 1; i < validBusses.Count; i++)
+            {
+                var (factorB, remainderB) = validBusses[i];
+                var bezoutCoefficients = ExtendedEuclideanAlgorithm(factorA, factorB);
+                var newRemainderA = remainderA * factorB * bezoutCoefficients.BezoutB + remainderB * factorA * bezoutCoefficients.BezoutA;
+                factorA = factorA * factorB;
+                remainderA = ((newRemainderA % factorA) + factorA) % factorA;
+            }
+            Debug.Assert(remainderA < long.MaxValue);
+            return (long)remainderA;
+        }
+
+        // https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode
+        private static (BigInteger BezoutA, BigInteger BezoutB) ExtendedEuclideanAlgorithm(BigInteger a, BigInteger b)
+        {
+            BigInteger old_r = a;
+            BigInteger r = b;
+            BigInteger old_s = 1;
+            BigInteger s = 0;
+            BigInteger old_t = 0;
+            BigInteger t = 1;
+            while (r != 0)
+            {
+                BigInteger quotient = old_r / r;
+                (old_r, r) = (r, old_r - quotient * r);
+                (old_s, s) = (s, old_s - quotient * s);
+                (old_t, t) = (t, old_t - quotient * t);
+            }
+            return (old_s, old_t);
+        }
+    }
+}
---
+++ Aoc2020Tests/Day13Tests.cs
@@ -0,0 +1,73 @@
+﻿namespace Aoc2020.Tests
+{
+    // https://adventofcode.com/2020/day/13
+    // --- Day 13: Shuttle Search ---
+    [TestClass()]
+    public class Day13Tests
+    {
+        [TestMethod()]
+        public void Day13_Part1_ExampleTest()
+        {
+            var instance = new Day13(File.ReadAllText("day13-example1.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(295, answer);
+        }
+        [TestMethod()]
+        public void Day13_Part1_InputTest()
+        {
+            var instance = new Day13(File.ReadAllText("day13-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(3789, answer);
+        }
+
+        [TestMethod()]
+        public void Day13_Part2_Example1Test()
+        {
+            var instance = new Day13(File.ReadAllText("day13-example1.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(1068781, answer);
+        }
+        [TestMethod()]
+        public void Day13_Part2_Example2Test()
+        {
+            var instance = new Day13(File.ReadAllText("day13-example2.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(3417, answer);
+        }
+        [TestMethod()]
+        public void Day13_Part2_Example3Test()
+        {
+            var instance = new Day13(File.ReadAllText("day13-example3.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(754018, answer);
+        }
+        [TestMethod()]
+        public void Day13_Part2_Example4Test()
+        {
+            var instance = new Day13(File.ReadAllText("day13-example4.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(779210, answer);
+        }
+        [TestMethod()]
+        public void Day13_Part2_Example5Test()
+        {
+            var instance = new Day13(File.ReadAllText("day13-example5.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(1261476, answer);
+        }
+        [TestMethod()]
+        public void Day13_Part2_Example6Test()
+        {
+            var instance = new Day13(File.ReadAllText("day13-example6.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(1202161486, answer);
+        }
+        [TestMethod()]
+        public void Day13_Part2_InputTest()
+        {
+            var instance = new Day13(File.ReadAllText("day13-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(667437230788118, answer);
+        }
+    }
+}
