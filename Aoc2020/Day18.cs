7cf368ad5874337b5265dca143834c040cbdd942
Raymond Trinh <rtrinh3@gmail.com>
2024-07-08 17:46:27 -0400

    2020 day 18

---
+++ Aoc2020/Day18.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Frozen;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Aoc2020
+{
+    // https://adventofcode.com/2020/day/18
+    // --- Day 18: Operation Order ---
+    public class Day18 : IAocDay
+    {
+        private readonly string[][] expressions;
+
+        public Day18(string input)
+        {
+            string[] lines = input.TrimEnd().Split('\n');
+            expressions = lines.Select(line =>
+            {
+                var matches = Regex.Matches(line, @"(\d+|\+|\*|\(|\))");
+                var tokens = matches.Cast<Match>().Select(m => m.Value);
+                return tokens.ToArray();
+            }).ToArray();
+        }
+
+        public long Part1()
+        {
+            Dictionary<string, int> partOnePrecedence = new Dictionary<string, int>()
+            {
+                ["+"] = 1,
+                ["*"] = 1,
+            };
+            var answer = expressions.Sum(expr => EvaluateRpn(ShuntingYardAlgorithm(expr, partOnePrecedence)));
+            return answer;
+        }
+
+        public long Part2()
+        {
+            Dictionary<string, int> partTwoPrecedence = new Dictionary<string, int>()
+            {
+                ["+"] = 2,
+                ["*"] = 1,
+            };
+            var answer = expressions.Sum(expr => EvaluateRpn(ShuntingYardAlgorithm(expr, partTwoPrecedence)));
+            return answer;
+        }
+
+        // https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+        private static IEnumerable<string> ShuntingYardAlgorithm(IEnumerable<string> infixExpression, IReadOnlyDictionary<string, int> precedence)
+        {
+            Stack<string> operatorStack = new();
+            foreach (string token in infixExpression)
+            {
+                if (token.All(char.IsAsciiDigit))
+                {
+                    yield return token;
+                }
+                else if (precedence.Keys.Contains(token))
+                {
+                    var tokenPrecedence = precedence[token];
+                    while (operatorStack.Count > 0 && operatorStack.Peek() != "(" && precedence[operatorStack.Peek()] >= tokenPrecedence)
+                    {
+                        yield return operatorStack.Pop();
+                    }
+                    operatorStack.Push(token);
+                }
+                else if (token == "(")
+                {
+                    operatorStack.Push(token);
+                }
+                else if (token == ")")
+                {
+                    Debug.Assert(operatorStack.Count >= 1, "Mismatched parentheses");
+                    while (operatorStack.Peek() != "(")
+                    {
+                        yield return operatorStack.Pop();
+                    }
+                    Debug.Assert(operatorStack.Count >= 1 && operatorStack.Peek() == "(", "Mismatched parentheses");
+                    operatorStack.Pop();
+                }
+            }
+            while (operatorStack.Count > 0)
+            {
+                yield return operatorStack.Pop();
+            }
+        }
+
+        private static long EvaluateRpn(IEnumerable<string> rpnExpression)
+        {
+            Stack<long> stack = new();
+            foreach (string token in rpnExpression)
+            {
+                if (token.All(char.IsAsciiDigit))
+                {
+                    stack.Push(long.Parse(token));
+                }
+                else if (token == "+")
+                {
+                    var operandA = stack.Pop();
+                    var operandB = stack.Pop();
+                    var newValue = operandA + operandB;
+                    stack.Push(newValue);
+                }
+                else if (token == "*")
+                {
+                    var operandA = stack.Pop();
+                    var operandB = stack.Pop();
+                    var newValue = operandA * operandB;
+                    stack.Push(newValue);
+                }
+            }
+            Debug.Assert(stack.Count >= 1);
+            return stack.Peek();
+        }
+    }
+}
---
+++ Aoc2020Tests/Day18Tests.cs
@@ -0,0 +1,108 @@
+﻿namespace Aoc2020.Tests
+{
+    // https://adventofcode.com/2020/day/18
+    // --- Day 18: Operation Order ---
+    [TestClass()]
+    public class Day18Tests
+    {
+        [TestMethod()]
+        public void Part1_Example1_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example1.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(71, answer);
+        }
+        [TestMethod()]
+        public void Part1_Example2_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example2.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(51, answer);
+        }
+        [TestMethod()]
+        public void Part1_Example3_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example3.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(26, answer);
+        }
+        [TestMethod()]
+        public void Part1_Example4_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example4.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(437, answer);
+        }
+        [TestMethod()]
+        public void Part1_Example5_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example5.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(12240, answer);
+        }
+        [TestMethod()]
+        public void Part1_Example6_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example6.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(13632, answer);
+        }
+        [TestMethod()]
+        public void Part1_Input_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(21993583522852, answer);
+        }
+
+        [TestMethod()]
+        public void Part2_Example1_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example1.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(231, answer);
+        }
+        [TestMethod()]
+        public void Part2_Example2_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example2.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(51, answer);
+        }
+        [TestMethod()]
+        public void Part2_Example3_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example3.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(46, answer);
+        }
+        [TestMethod()]
+        public void Part2_Example4_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example4.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(1445, answer);
+        }
+        [TestMethod()]
+        public void Part2_Example5_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example5.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(669060, answer);
+        }
+        [TestMethod()]
+        public void Part2_Example6_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-example6.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(23340, answer);
+        }
+        [TestMethod()]
+        public void Part2_Input_Test()
+        {
+            var instance = new Day18(File.ReadAllText("day18-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(122438593522757, answer);
+        }
+    }
+}
