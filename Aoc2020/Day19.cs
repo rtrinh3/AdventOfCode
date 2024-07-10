5757dbd035acf9b64fe93bf2c4482a57b2adb563
Raymond Trinh <rtrinh3@gmail.com>
2024-07-09 20:22:50 -0400

    2020 day 19 part 1

---
+++ Aoc2020/Day19.cs
@@ -0,0 +1,69 @@
+﻿namespace Aoc2020
+{
+    // https://adventofcode.com/2020/day/19
+    // --- Day 19: Monster Messages ---
+    public class Day19 : IAocDay
+    {
+        private readonly Dictionary<string, Func<string, int, int>> rules;
+        private readonly string[] messages;
+
+        public Day19(string input)
+        {
+            var parts = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+            string rulesPart = parts[0];
+            var rulesLines = rulesPart.Split('\n');
+            rules = new();
+            foreach (var line in rulesLines)
+            {
+                var ruleNumberSplit = line.Split(':');
+                string ruleNumber = ruleNumberSplit[0].Trim();
+                var alternativesSplit = ruleNumberSplit[1].Split('|');
+                var alternatives = alternativesSplit.Select(alt => alt.Trim().Split(' ')).ToArray();
+                rules[ruleNumber] = (string message, int index) =>
+                {
+                    foreach (var alt in alternatives)
+                    {
+                        int current = index;
+                        foreach (var subrule in alt)
+                        {
+                            current = rules[subrule](message, current);
+                            if (current < 0)
+                            {
+                                break;
+                            }
+                        }
+                        if (current >= index)
+                        {
+                            return current;
+                        }
+                    }
+                    return -1;
+                };
+            }
+            rules["\"a\""] = (string message, int index) => message[index] == 'a' ? index + 1 : -1;
+            rules["\"b\""] = (string message, int index) => message[index] == 'b' ? index + 1 : -1;
+
+            string messagesPart = parts[1];
+            messages = messagesPart.Split('\n');
+        }
+
+        public long Part1()
+        {
+            int answer = 0;
+            foreach (string message in messages)
+            {
+                var test = rules["0"](message, 0);
+                if (test == message.Length)
+                {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+
+        public long Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Aoc2020Tests/Day19Tests.cs
@@ -0,0 +1,46 @@
+﻿namespace Aoc2020.Tests
+{
+    // https://adventofcode.com/2020/day/19
+    // --- Day 19: Monster Messages ---
+    [TestClass()]
+    public class Day19Tests
+    {
+        [TestMethod()]
+        public void Day19_Part1_Example1Test()
+        {
+            var instance = new Day19(File.ReadAllText("day19-example1.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(2, answer);
+        }
+        [TestMethod()]
+        public void Day19_Part1_Example2Test()
+        {
+            var instance = new Day19(File.ReadAllText("day19-example2.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(3, answer);
+        }
+        [TestMethod()]
+        public void Day19_Part1_InputTest()
+        {
+            var instance = new Day19(File.ReadAllText("day19-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(107, answer);
+        }
+
+        [TestMethod()]
+        public void Day19_Part2_ExampleTest()
+        {
+            var instance = new Day19(File.ReadAllText("day19-example2.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(12, answer);
+        }
+        [TestMethod()]
+        public void Day19_Part2_InputTest()
+        {
+            var instance = new Day19(File.ReadAllText("day19-input.txt"));
+            var answer = instance.Part2();
+            //Assert.AreEqual(1319, answer);
+            Assert.Inconclusive(answer.ToString());
+        }
+    }
+}
