f18670d2fb7824f6fa3914905c151b1ff9dec0c4
Raymond Trinh <rtrinh3@gmail.com>
2024-07-11 17:35:09 -0400

    2020 day 20 part 1

---
+++ Aoc2020/Day20.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics;
+
+namespace Aoc2020
+{
+    // https://adventofcode.com/2020/day/20
+    // --- Day 20: Jurassic Jigsaw ---
+    public class Day20 : IAocDay
+    {
+        private readonly List<Tile> tiles;
+
+        public Day20(string input)
+        {
+            tiles = new();
+            string[] tileData = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+            foreach (var tile in tileData)
+            {
+                var split = tile.Split(":\n");
+                int id = int.Parse(split[0].Split(' ')[1]);
+                string[] data = split[1].Split('\n');
+                tiles.Add(new Tile(id, data));
+            }
+
+            for (int i = 0; i < tiles.Count; i++)
+            {
+                for (int j = i + 1; j < tiles.Count; j++)
+                {
+                    if (tiles[i].Edges.Intersect(tiles[j].AllEdges).Any())
+                    {
+                        tiles[i].Matches.Add(tiles[j].ID);
+                        tiles[j].Matches.Add(tiles[i].ID);
+                    }
+                }
+            }
+        }
+
+        public long Part1()
+        {
+            var corners = tiles.Where(t => t.Matches.Count == 2).ToList();
+            Debug.Assert(corners.Count == 4);
+            long product = (long)corners[0].ID * corners[1].ID * corners[2].ID * corners[3].ID;
+            return product;
+        }
+
+        public long Part2()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Tile
+        {
+            public int ID { get; init; }
+            public string[] Image { get; init; }
+            public List<int> Matches { get; init; }
+            public IEnumerable<ushort> Edges => [_edges.Top, _edges.Right, _edges.Bottom, _edges.Left];
+            public IEnumerable<ushort> AllEdges => [_edges.Top, _edges.Right, _edges.Bottom, _edges.Left, FlipEdge(_edges.Top), FlipEdge(_edges.Left), FlipEdge(_edges.Bottom), FlipEdge(_edges.Right)];
+            private readonly (ushort Top, ushort Right, ushort Bottom, ushort Left) _edges;
+
+            public Tile(int id, string[] image)
+            {
+                ID = id;
+                Image = image;
+                Matches = new();
+                ushort top = ConvertToEdge(image[0]);
+                ushort right = ConvertToEdge(image.Select(line => line.Last()));
+                ushort bottom = FlipEdge(ConvertToEdge(image.Last()));
+                ushort left = ConvertToEdge(image.Reverse().Select(line => line[0]));
+                _edges = (top, right, bottom, left);
+            }
+
+            public static ushort FlipEdge(ushort edge)
+            {
+                ushort answer = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    if ((edge & (1U << i)) != 0)
+                    {
+                        answer |= (ushort)(1U << (10 - 1 - i));
+                    }
+                }
+                return answer;
+            }
+
+            private static ushort ConvertToEdge(IEnumerable<char> edgeChars)
+            {
+                ushort answer = 0;
+                int i = 0;
+                foreach (char c in edgeChars)
+                {
+                    answer <<= 1;
+                    if (c == '#')
+                    {
+                        answer |= 1;
+                    }
+                    i++;
+                }
+                return answer;
+            }
+        }
+    }
+}
---
+++ Aoc2020Tests/Day20Tests.cs
@@ -0,0 +1,39 @@
+﻿namespace Aoc2020.Tests
+{
+    // https://adventofcode.com/2020/day/20
+    // --- Day 20: Jurassic Jigsaw ---
+    [TestClass()]
+    public class Day20Tests
+    {
+        [TestMethod()]
+        public void Day20_Part1_ExampleTest()
+        {
+            var instance = new Day20(File.ReadAllText("day20-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(20899048083289, answer);
+        }
+        [TestMethod()]
+        public void Day20_Part1_InputTest()
+        {
+            var instance = new Day20(File.ReadAllText("day20-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(54755174472007, answer);
+        }
+
+        [TestMethod()]
+        public void Day20_Part2_ExampleTest()
+        {
+            var instance = new Day20(File.ReadAllText("day20-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual(2, answer);
+        }
+        [TestMethod()]
+        public void Day20_Part2_InputTest()
+        {
+            var instance = new Day20(File.ReadAllText("day20-input.txt"));
+            var answer = instance.Part2();
+            //Assert.AreEqual(107, answer);
+            Assert.Inconclusive(answer.ToString());
+        }
+    }
+}
