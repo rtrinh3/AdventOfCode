a0866a2bd336234e4ecf908a6d5ec6b1efcf5113
Raymond Trinh <rtrinh3@gmail.com>
2024-07-12 13:15:27 -0400

    2020 day 25

---
+++ Aoc2020/Day25.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace Aoc2020
+{
+    // https://adventofcode.com/2020/day/25
+    // --- Day 25: Combo Breaker ---
+    public class Day25(string input) : IAocDay
+    {
+        private const int INITIAL_SUBJECT_NUMBER = 7;
+        private const int MODULUS = 20201227;
+
+        private static int GetLoopSize(int publicKey)
+        {
+            int value = 1;
+            int loopSize = 0;
+            while (value != publicKey)
+            {
+                // Transform
+                value = (int)(Math.BigMul(value, INITIAL_SUBJECT_NUMBER) % MODULUS);
+                loopSize++;
+            }
+            return loopSize;
+        }
+
+        private static int Transform(int value, int loop)
+        {
+            int ret = 1;
+            for (int i = 0; i < loop; i++)
+            {
+                ret = (int)(Math.BigMul(ret, value) % MODULUS);
+            }
+            return ret;
+        }
+
+        public long Part1()
+        {
+            string[] keys = input.Split('\n');
+            int cardPublicKey = int.Parse(keys[0]);
+            int doorPublicKey = int.Parse(keys[1]);
+            int cardLoopSize = GetLoopSize(cardPublicKey);
+            int doorLoopSize= GetLoopSize(doorPublicKey);
+            int cardDoorEncryptionKey = Transform(cardPublicKey, doorLoopSize);
+            int doorCardEncryptionKey = Transform(doorPublicKey, cardLoopSize);
+            Debug.Assert(cardDoorEncryptionKey == doorCardEncryptionKey);
+            return cardDoorEncryptionKey;
+        }
+
+        public long Part2()
+        {
+            return MODULUS;
+        }
+    }
+}
---
+++ Aoc2020Tests/Day25Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace Aoc2020.Tests
+{
+    // https://adventofcode.com/2020/day/25
+    // --- Day 25: Combo Breaker ---
+    [TestClass()]
+    public class Day25Tests
+    {
+        [TestMethod()]
+        public void Day25_Example_Test()
+        {
+            var instance = new Day25(File.ReadAllText("day25-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(14897079, answer);
+        }
+
+        [TestMethod()]
+        public void Day25_Input_Test()
+        {
+            var instance = new Day25(File.ReadAllText("day25-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual(16933668, answer);
+        }
+    }
+}
