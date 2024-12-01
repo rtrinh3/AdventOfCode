0c774b04e2ff5a291da4365bb0500d358c554de5
Raymond Trinh <rtrinh3@gmail.com>
2024-12-01 07:56:01 -0500

    2024 day 1

--- Aoc2024/Day01.cs
+++ Aoc2024/Day01.cs
@@ -1,15 +1,51 @@
-﻿namespace Aoc2024
+﻿using System.Diagnostics;
+
+namespace Aoc2024
 {
+    // https://adventofcode.com/2024/day/1
+    // Day 1: Historian Hysteria
     public class Day01: AocCommon.IAocDay
     {
+        private readonly long[] listA;
+        private readonly long[] listB;
+
+        public Day01(string input)
+        {
+            List<long> listABuffer = new();
+            List<long> listBBuffer = new();
+            foreach (string line in input.ReplaceLineEndings("\n").TrimEnd().Split('\n'))
+            {
+                var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                Debug.Assert(parts.Length == 2);
+                listABuffer.Add(long.Parse(parts[0]));
+                listBBuffer.Add(long.Parse(parts[1]));
+            }
+            listA = listABuffer.ToArray();
+            listB = listBBuffer.ToArray();
+        }
+
         public string Part1()
         {
-            throw new NotImplementedException();
+            var sortedA = listA.Order().ToList();
+            var sortedB = listB.Order().ToList();
+            Debug.Assert(sortedA.Count == sortedB.Count);
+            long totalDistance = 0;
+            for (int i = 0; i < sortedA.Count; i++)
+            {
+                totalDistance += Math.Abs(sortedA[i] - sortedB[i]);
+            }
+            return totalDistance.ToString();
         }
 
         public string Part2()
         {
-            throw new NotImplementedException();
+            Dictionary<long, int> leftEntries = listA.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+            long totalSimilarity = 0;
+            foreach (long n in listB)
+            {
+                totalSimilarity += n * leftEntries.GetValueOrDefault(n, 0);
+            }
+            return totalSimilarity.ToString();
         }
     }
 }
--- Aoc2024Tests/Day01Tests.cs
+++ Aoc2024Tests/Day01Tests.cs
@@ -1,18 +1,40 @@
-﻿namespace Aoc2024.Tests
+﻿using Aoc2024;
+
+namespace Aoc2014.Tests
 {
+    // https://adventofcode.com/2024/day/1
+    // Day 1: Historian Hysteria
     [TestClass()]
     public class Day01Tests
     {
         [TestMethod()]
-        public void Day01_Part1_Test()
+        public void Part1ExampleTest()
         {
-            Assert.Fail();
+            var instance = new Day01(File.ReadAllText("day01-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("11", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day01(File.ReadAllText("day01-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("2378066", answer);
         }
 
         [TestMethod()]
-        public void Day01_Part2_Test()
+        public void Part2ExampleTest()
         {
-            Assert.Fail();
+            var instance = new Day01(File.ReadAllText("day01-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("31", answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day01(File.ReadAllText("day01-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("18934359", answer);
         }
     }
 }
