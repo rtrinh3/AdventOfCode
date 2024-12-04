0b228109b9ffc2dcc44b6608c1fd2c81827f4caa
Raymond Trinh <rtrinh3@gmail.com>
2024-12-04 08:09:12 -0500

    2024 day 4

---
+++ Aoc2024/Day04.cs
@@ -0,0 +1,72 @@
+﻿using AocCommon;
+
+namespace Aoc2024
+{
+    // https://adventofcode.com/2024/day/4
+    // --- Day 4: Ceres Search ---
+    public class Day04(string input) : IAocDay
+    {
+        private readonly Grid grid = new(input, '\0');
+
+        public string Part1()
+        {
+            VectorRC[][] directions =
+            [
+                [new(0, +1), new(0, +2), new(0, +3)],
+                [new(0, -1), new(0, -2), new(0, -3)],
+                [new(+1, 0), new(+2, 0), new(+3, 0)],
+                [new(-1, 0), new(-2, 0), new(-3, 0)],
+                [new(+1, +1), new(+2, +2), new(+3, +3)],
+                [new(-1, -1), new(-2, -2), new(-3, -3)],
+                [new(+1, -1), new(+2, -2), new(+3, -3)],
+                [new(-1, +1), new(-2, +2), new(-3, +3)],
+            ];
+            long count = 0;
+            foreach (var (Pos, Value) in grid.Iterate())
+            {
+                if (Value == 'X')
+                {
+                    foreach (var dir in directions)
+                    {
+                        if (grid.Get(Pos + dir[0]) == 'M' && grid.Get(Pos + dir[1]) == 'A' && grid.Get(Pos + dir[2]) == 'S')
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count.ToString();
+        }
+
+        public string Part2()
+        {
+            VectorRC[][] directions =
+            [
+                [new (+1, +1), new(-1, -1)],
+                [new (-1, -1), new(+1, +1)],
+                [new (+1, -1), new(-1, +1)],
+                [new (-1, +1), new(+1, -1)],
+            ];
+            long count = 0;
+            foreach (var (Pos, Value) in grid.Iterate())
+            {
+                if (Value == 'A')
+                {
+                    int crossCount = 0;
+                    foreach (var dir in directions)
+                    {
+                        if (grid.Get(Pos + dir[0]) == 'M' && grid.Get(Pos + dir[1]) == 'S')
+                        {
+                            crossCount++;
+                        }
+                    }
+                    if (crossCount == 2)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count.ToString();
+        }
+    }
+}
---
+++ Aoc2024Tests/Day04Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace Aoc2024.Tests
+{
+    // https://adventofcode.com/2024/day/4
+    // --- Day 4: Ceres Search ---
+    [TestClass()]
+    public class Day04Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day04(File.ReadAllText("day04-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("18", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day04(File.ReadAllText("day04-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("2560", answer);
+        }
+
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var instance = new Day04(File.ReadAllText("day04-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("9", answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day04(File.ReadAllText("day04-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("1910", answer);
+        }
+    }
+}
