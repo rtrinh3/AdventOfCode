7a547dbcb924b07551e7f6ac6518185f344af28e
Raymond Trinh <rtrinh3@gmail.com>
2024-12-05 13:25:30 +0000

    2024 day 5 part 1

---
+++ Aoc2024/Day05.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using AocCommon;
+
+namespace Aoc2024
+{
+    // https://adventofcode.com/2024/day/5
+    // --- Day 5: Print Queue ---
+    public class Day05(string input) : IAocDay
+    {
+        public string Part1()
+        {
+            // Parse
+            var paragraphs = input.ReplaceLineEndings("\n").TrimEnd().Split("\n\n");
+            var orderLines = paragraphs[0].Split('\n');
+            Dictionary<long, HashSet<long>> beforeAfter = orderLines
+                .Select(line => line.Split('|').Select(long.Parse).ToArray())
+                .GroupBy(line => line[0])
+                .ToDictionary(g => g.Key, g => g.Select(line => line[1]).ToHashSet());
+
+            var printLines = paragraphs[1].Split('\n');
+            long[][] prints = printLines.Select(line => line.Split(',').Select(long.Parse).ToArray()).ToArray();
+
+            //
+            List<long[]> correctPrints = new();
+            foreach (var print in prints)
+            {
+                for (int i = 0; i < print.Length - 1; i++) 
+                {
+                    for (int j = i + 1; j < print.Length; j++)
+                    {
+                        if (beforeAfter.ContainsKey(print[j]) && beforeAfter[print[j]].Contains(print[i])) 
+                        {
+                            goto INCORRECT;
+                        }
+                    }
+                }
+                correctPrints.Add(print);
+                INCORRECT:;
+            }
+            long sum = 0;
+            foreach (var print in correctPrints)
+            {
+                Debug.Assert(print.Length % 2 == 1);
+                var middle = print[print.Length / 2];
+                sum += middle;
+            }
+
+            return sum.ToString();
+        }
+
+        public string Part2()
+        {
+            return "NO";
+        }
+    }
+}
---
+++ Aoc2024Tests/Day05Tests.cs
@@ -0,0 +1,40 @@
+﻿namespace Aoc2024.Tests
+{
+    // https://adventofcode.com/2024/day/5
+    // --- Day 5: Print Queue ---
+    [TestClass()]
+    public class Day05Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day05(File.ReadAllText("day05-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("143", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day05(File.ReadAllText("day05-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("5329", answer);
+        }
+
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var instance = new Day05(File.ReadAllText("day05-example.txt"));
+            var answer = instance.Part2();
+            //Assert.AreEqual("18", answer);
+            Assert.Inconclusive(answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day05(File.ReadAllText("day05-input.txt"));
+            var answer = instance.Part2();
+            //Assert.AreEqual("18", answer);
+            Assert.Inconclusive(answer);
+        }
+    }
+}
