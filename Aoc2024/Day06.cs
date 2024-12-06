33f1780ac001548d401b5e88792d17238511d3cf
Raymond Trinh <rtrinh3@gmail.com>
2024-12-06 08:06:59 -0500

    2024 day 6 part 1

---
+++ Aoc2024/Day06.cs
@@ -0,0 +1,47 @@
+﻿using AocCommon;
+
+namespace Aoc2024
+{
+    // https://adventofcode.com/2024/day/6
+    // --- Day 6: Guard Gallivant ---
+    public class Day06(string input) : IAocDay
+    {
+        private const char OUTSIDE = '\0';
+        private readonly Grid grid = new(input, OUTSIDE);
+
+        public string Part1()
+        {
+            var startPos = grid.Iterate().Where(x => x.Value == '^').Select(x => x.Position).Single();
+            var startDir = VectorRC.Up;
+
+            HashSet<VectorRC> visited = new();
+            VectorRC pos = startPos;
+            VectorRC dir = startDir;
+            char tile;
+            while (OUTSIDE != (tile = grid.Get(pos)))
+            {
+                visited.Add(pos);
+                VectorRC nextDir = dir;
+                while (true)
+                {
+                    VectorRC nextPos = pos + nextDir;
+                    char nextTile = grid.Get(nextPos);
+                    if (nextTile != '#')
+                    {
+                        break;
+                    }
+                    nextDir = nextDir.RotatedRight();
+                }
+                pos = pos + nextDir;
+                dir = nextDir;
+            }
+
+            return visited.Count.ToString();
+        }
+
+        public string Part2()
+        {
+            return "2";
+        }
+    }
+}
---
+++ Aoc2024Tests/Day06Tests.cs
@@ -0,0 +1,40 @@
+﻿namespace Aoc2024.Tests
+{
+    // https://adventofcode.com/2024/day/6
+    // --- Day 6: Guard Gallivant ---
+    [TestClass()]
+    public class Day06Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("41", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("4982", answer);
+        }
+
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-example.txt"));
+            var answer = instance.Part2();
+            //Assert.AreEqual("5329", answer);
+            Assert.Inconclusive(answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day06(File.ReadAllText("day06-input.txt"));
+            var answer = instance.Part2();
+            //Assert.AreEqual("5329", answer);
+            Assert.Inconclusive(answer);
+        }
+    }
+}
