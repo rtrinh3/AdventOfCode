bd86037beacf9751f5b4cb5a5a026cd439fa1f28
Raymond Trinh <rtrinh3@gmail.com>
2024-12-08 08:19:07 -0500

    2024 day 08

---
+++ Aoc2024/Day08.cs
@@ -0,0 +1,94 @@
+﻿using AocCommon;
+
+namespace Aoc2024
+{
+    // https://adventofcode.com/2024/day/8
+    // --- Day 8: Resonant Collinearity ---
+    public class Day08 : IAocDay
+    {
+        private const char OUTSIDE = '\0';
+
+        private readonly Grid map;
+        private readonly DefaultDict<char, List<VectorRC>> antennasByFrequency;
+
+        public Day08(string input)
+        {
+            map = new Grid(input, OUTSIDE);
+            antennasByFrequency = new();
+            foreach (var (Position, Value) in map.Iterate())
+            {
+                if (char.IsAsciiLetterOrDigit(Value))
+                {
+                    antennasByFrequency[Value].Add(Position);
+                }
+            }
+        }
+
+        private bool InBounds(VectorRC x)
+        {
+            return 0 <= x.Row && x.Row < map.Height && 0 <= x.Col && x.Col < map.Width;
+        }
+
+        public string Part1()
+        {
+            HashSet<VectorRC> antinodes = new();
+            foreach (var (_, Positions) in antennasByFrequency)
+            {
+                if (Positions.Count >= 2)
+                {
+                    for (int i = 0; i < Positions.Count - 1; i++)
+                    {
+                        for (int j = i + 1; j < Positions.Count; j++)
+                        {
+                            var difference = Positions[j] - Positions[i];
+                            var antinodeA = Positions[j] + difference;
+                            if (InBounds(antinodeA))
+                            {
+                                antinodes.Add(antinodeA);
+                            }
+                            var antinodeB = Positions[i] - difference;
+                            if (InBounds(antinodeB))
+                            {
+                                antinodes.Add(antinodeB);
+                            }
+                        }
+                    }
+                }
+            }
+            return antinodes.Count.ToString();
+        }
+
+        public string Part2()
+        {
+            HashSet<VectorRC> antinodes = new();
+            foreach (var (_, Positions) in antennasByFrequency)
+            {
+                if (Positions.Count >= 2)
+                {
+                    for (int i = 0; i < Positions.Count - 1; i++)
+                    {
+                        for (int j = i + 1; j < Positions.Count; j++)
+                        {
+                            var difference = Positions[j] - Positions[i];
+                            var gcd = MoreMath.Gcd(Math.Abs(difference.Row), Math.Abs(difference.Col));
+                            var slope = new VectorRC(difference.Row / gcd, difference.Col / gcd);
+                            var positive = Positions[i];
+                            while (InBounds(positive))
+                            {
+                                antinodes.Add(positive);
+                                positive += slope;
+                            }
+                            var negative = Positions[i];
+                            while (InBounds(negative))
+                            {
+                                antinodes.Add(negative);
+                                negative -= slope;
+                            }
+                        }
+                    }
+                }
+            }
+            return antinodes.Count.ToString();
+        }
+    }
+}
---
+++ Aoc2024Tests/Day08Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace Aoc2024.Tests
+{
+    // https://adventofcode.com/2024/day/8
+    // --- Day 8: Resonant Collinearity ---
+    [TestClass()]
+    public class Day08Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day08(File.ReadAllText("day08-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("14", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day08(File.ReadAllText("day08-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("332", answer);
+        }
+
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var instance = new Day08(File.ReadAllText("day08-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("34", answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day08(File.ReadAllText("day08-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("1174", answer);
+        }
+    }
+}
