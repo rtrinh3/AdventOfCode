c5d30f01675ad7ea066a3e7393b6d01cd2808bef
Raymond Trinh <rtrinh3@gmail.com>
2024-12-10 07:19:24 -0500

    2024 day 10

---
+++ Aoc2024/Day10.cs
@@ -0,0 +1,76 @@
+﻿using AocCommon;
+
+namespace Aoc2024
+{
+    public class Day10(string input) : IAocDay
+    {
+        private const char OUTSIDE = '\0';
+        private readonly Grid map = new(input, OUTSIDE);
+
+        public string Part1()
+        {
+            Dictionary<VectorRC, int> trailheadScores = new();
+            foreach (var (Position, Value) in map.Iterate())
+            {
+                if (Value != '0')
+                {
+                    continue;
+                }
+                HashSet<VectorRC> summits = new();
+                void Visit(VectorRC step)
+                {
+                    var current = map.Get(step);
+                    if (current == '9')
+                    {
+                        summits.Add(step);
+                        return;
+                    }
+                    foreach (var next in step.NextFour())
+                    {
+                        if (map.Get(next) == current + 1)
+                        {
+                            Visit(next);
+                        }
+                    }
+                }
+                Visit(Position);
+                trailheadScores[Position] = summits.Count;
+            }
+            var answer = trailheadScores.Values.Sum();
+            return answer.ToString();
+        }
+
+        public string Part2()
+        {
+            Dictionary<VectorRC, int> trailheadRatings = new();
+            foreach (var (Position, Value) in map.Iterate())
+            {
+                if (Value != '0')
+                {
+                    continue;
+                }
+                int trails = 0;
+                void Visit(VectorRC step)
+                {
+                    var current = map.Get(step);
+                    if (current == '9')
+                    {
+                        trails++;
+                        return;
+                    }
+                    foreach (var next in step.NextFour())
+                    {
+                        if (map.Get(next) == current + 1)
+                        {
+                            Visit(next);
+                        }
+                    }
+                }
+                Visit(Position);
+                trailheadRatings[Position] = trails;
+            }
+            var answer = trailheadRatings.Values.Sum();
+            return answer.ToString();
+        }
+    }
+}
---
+++ Aoc2024Tests/Day10Tests.cs
@@ -0,0 +1,36 @@
+﻿namespace Aoc2024.Tests
+{
+    [TestClass()]
+    public class Day10Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day10(File.ReadAllText("day10-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("36", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day10(File.ReadAllText("day10-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("430", answer);
+        }
+
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var instance = new Day10(File.ReadAllText("day10-example.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("81", answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day10(File.ReadAllText("day10-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("928", answer);
+        }
+    }
+}
