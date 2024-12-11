de0238f43a24cd55d942cabde268fec63289a8e6
Raymond Trinh <rtrinh3@gmail.com>
2024-12-11 08:12:14 -0500

    2024 day 11 part 1

---
+++ Aoc2024/Day11.cs
@@ -0,0 +1,58 @@
+﻿using AocCommon;
+
+namespace Aoc2024
+{
+    // https://adventofcode.com/2024/day/11
+    // --- Day 11: Plutonian Pebbles ---
+    public class Day11(string input) : IAocDay
+    {
+        private static string NormalizeStone(string stone)
+        {
+            stone = stone.TrimStart('0');
+            if (stone == "")
+            {
+                return "0";
+            }
+            else
+            {
+                return stone;
+            }
+        }
+
+        public string Part1()
+        {
+            List<string> stones = input.TrimEnd().Split(' ').ToList();
+            for (int i = 0; i < 25; i++)
+            {
+                List<string> nextStones = new();
+                foreach (var stone in stones)
+                {
+                    if (stone == "0")
+                    {
+                        nextStones.Add("1");
+                    }
+                    else if (stone.Length % 2 == 0)
+                    {
+                        string left = NormalizeStone(stone.Substring(0, stone.Length / 2));
+                        nextStones.Add(left);
+                        string right = NormalizeStone(stone.Substring(stone.Length / 2, stone.Length / 2));
+                        nextStones.Add(right);
+                    }
+                    else
+                    {
+                        var value = long.Parse(stone);
+                        var nextValue = value * 2024;
+                        nextStones.Add(nextValue.ToString());
+                    }
+                }
+                stones = nextStones;
+            }
+            return stones.Count.ToString();
+        }
+
+        public string Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Aoc2024Tests/Day11Tests.cs
@@ -0,0 +1,40 @@
+﻿namespace Aoc2024.Tests
+{
+    // https://adventofcode.com/2024/day/11
+    // --- Day 11: Plutonian Pebbles ---
+    [TestClass()]
+    public class Day11Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day11(File.ReadAllText("day11-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("55312", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day11(File.ReadAllText("day11-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("198089", answer);
+        }
+
+        //[TestMethod()]
+        //public void Part2ExampleTest()
+        //{
+        //    var instance = new Day11(File.ReadAllText("day11-example.txt"));
+        //    var answer = instance.Part2();
+        //    //Assert.AreEqual("430", answer);
+        //    Assert.Inconclusive(answer);
+        //}
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day11(File.ReadAllText("day11-input.txt"));
+            var answer = instance.Part2();
+            //Assert.AreEqual("430", answer);
+            Assert.Inconclusive(answer);
+        }
+    }
+}
