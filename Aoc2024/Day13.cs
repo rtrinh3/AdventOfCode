3156c3eace4e6dcaf408694800c57144b13bcc35
Raymond Trinh <rtrinh3@gmail.com>
2024-12-13 08:16:48 -0500

    2024 day 13

---
+++ Aoc2024/Day13.cs
@@ -0,0 +1,59 @@
+﻿using AocCommon;
+using MathNet.Numerics.LinearAlgebra;
+using System.Text.RegularExpressions;
+
+namespace Aoc2024
+{
+    // https://adventofcode.com/2024/day/13
+    // --- Day 13: Claw Contraption ---
+    public class Day13(string input) : IAocDay
+    {
+        public string Part1()
+        {
+            var paragraphs = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+            double counter = 0;
+            foreach (var p in paragraphs)
+            {
+                var m = Regex.Match(p, @"^Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)$");
+                double[] ba = [double.Parse(m.Groups[1].ValueSpan), double.Parse(m.Groups[2].ValueSpan)];
+                double[] bb = [double.Parse(m.Groups[3].ValueSpan), double.Parse(m.Groups[4].ValueSpan)];
+                double tx = double.Parse(m.Groups[5].ValueSpan);
+                double ty = double.Parse(m.Groups[6].ValueSpan);
+                Matrix<double> eqs = Matrix<double>.Build.DenseOfColumnArrays(ba, bb);
+                Vector<double> ts = Vector<double>.Build.Dense([tx, ty]);
+                var solve = eqs.Solve(ts);
+                var round = solve.Select(x => Math.Round(x, 3)).ToList();
+                if (round.All(x => x != 0 && x % 1 == 0))
+                {
+                    counter += round[0] * 3 + round[1] * 1;
+                }
+            }
+
+            return counter.ToString();
+        }
+
+        public string Part2()
+        {
+            var paragraphs = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+            double counter = 0;
+            foreach (var p in paragraphs)
+            {
+                var m = Regex.Match(p, @"^Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)$");
+                double[] ba = [double.Parse(m.Groups[1].ValueSpan), double.Parse(m.Groups[2].ValueSpan)];
+                double[] bb = [double.Parse(m.Groups[3].ValueSpan), double.Parse(m.Groups[4].ValueSpan)];
+                double tx = double.Parse(m.Groups[5].ValueSpan) + 10000000000000;
+                double ty = double.Parse(m.Groups[6].ValueSpan) + 10000000000000;
+                Matrix<double> eqs = Matrix<double>.Build.DenseOfColumnArrays(ba, bb);
+                Vector<double> ts = Vector<double>.Build.Dense([tx, ty]);
+                var solve = eqs.Solve(ts);
+                var round = solve.Select(x => Math.Round(x, 3)).ToList();
+                if (round.All(x => x != 0 && x % 1 == 0))
+                {
+                    counter += round[0] * 3 + round[1] * 1;
+                }
+            }
+
+            return counter.ToString();
+        }
+    }
+}
---
+++ Aoc2024Tests/Day13Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace Aoc2024.Tests
+{
+    // https://adventofcode.com/2024/day/13
+    // --- Day 13: Claw Contraption ---
+    [TestClass()]
+    public class Day13Tests
+    {
+        [TestMethod()]
+        public void Part1ExampleTest()
+        {
+            var instance = new Day13(File.ReadAllText("day13-example.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("480", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day13(File.ReadAllText("day13-input.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("37128", answer);
+        }
+
+        [TestMethod(), Timeout(15_000)]
+        public void Part2InputTest()
+        {
+            var instance = new Day13(File.ReadAllText("day13-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("74914228471331", answer);
+        }
+    }
+}
