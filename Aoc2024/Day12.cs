ebebec2de58b64897c44a14908810785b0614900
Raymond Trinh <rtrinh3@gmail.com>
2024-12-12 20:29:26 -0500

    2024 day 12 completed

--- Aoc2024/Day12.cs
+++ Aoc2024/Day12.cs
@@ -70,123 +70,6 @@ namespace Aoc2024
             return total.ToString();
         }
 
-        // Fence is defined by the two plots separated by the fence. The constructor normalizes the order.
-        public class Fence : IEquatable<Fence>
-        {
-            public VectorRC A { get; init; }
-            public VectorRC B { get; init; }
-
-            internal Fence(VectorRC X, VectorRC Y)
-            {
-                if (X == Y)
-                {
-                    throw new Exception("Must be distinct!");
-                }
-                if (Math.Abs(X.Row - Y.Row) >= 2 || Math.Abs(X.Col - Y.Col) >= 2)
-                {
-                    throw new Exception("Unaligned!");
-                }
-                if (Math.Abs(X.Row - Y.Row) == 1 && Math.Abs(X.Col - Y.Col) == 1)
-                {
-                    throw new Exception("Unaligned!");
-                }
-                Debug.Assert(Math.Abs(X.Row - Y.Row) == 0 || Math.Abs(X.Col - Y.Col) == 0);
-                if (Math.Abs(X.Row - Y.Row) == 0)
-                {
-                    Debug.Assert(Math.Abs(X.Col - Y.Col) == 1);
-                    if (X.Col < Y.Col)
-                    {
-                        A = X;
-                        B = Y;
-                    }
-                    else
-                    {
-                        A = Y;
-                        B = X;
-                    }
-                }
-                else
-                {
-                    Debug.Assert(Math.Abs(X.Col - Y.Col) == 0);
-                    Debug.Assert(Math.Abs(X.Row - Y.Row) == 1);
-                    if (X.Row < Y.Row)
-                    {
-                        A = X;
-                        B = Y;
-                    }
-                    else
-                    {
-                        A = Y;
-                        B = X;
-                    }
-                }
-            }
-
-            internal bool IsHorizontal => A.Col == B.Col;
-            internal bool IsVertical => A.Row == B.Row;
-
-            internal bool AreAdjacentAligned(Fence other)
-            {
-                if (this == other)
-                {
-                    return false;
-                }
-                if (this.IsHorizontal)
-                {
-                    if (!other.IsHorizontal)
-                    {
-                        return false;
-                    }
-                    return this.A.Row == other.A.Row && Math.Abs(this.A.Col - other.A.Col) == 1;
-                }
-                else if (this.IsVertical)
-                {
-                    if (!other.IsVertical)
-                    {
-                        return false;
-                    }
-                    return this.A.Col == other.A.Col && Math.Abs(this.A.Row - other.A.Row) == 1;
-                }
-                else
-                {
-                    throw new Exception("What orientation?");
-                }
-            }
-
-            // IEquatable
-            public bool Equals(Fence? other)
-            {
-                return ReferenceEquals(this, other) || (other is not null && this.A == other.A && this.B == other.B);
-            }
-
-            public override bool Equals(object? obj)
-            {
-                return Equals(obj as Fence);
-            }
-
-            public override int GetHashCode()
-            {
-                var hasher = new HashCode();
-                hasher.Add(A);
-                hasher.Add(B);
-                return hasher.ToHashCode();
-            }
-
-            public override string ToString()
-            {
-                return $"Fence({A}|{B})";
-            }
-
-            public static bool operator ==(Fence? lhs, Fence? rhs)
-            {
-                return lhs?.Equals(rhs) ?? (lhs is null && rhs is null);
-            }
-            public static bool operator !=(Fence? lhs, Fence? rhs)
-            {
-                return !(lhs == rhs);
-            }
-        }
-
         public string Part2()
         {
             // Partition the map and assign fences to plots
@@ -239,23 +122,153 @@ namespace Aoc2024
                 var type = map.Get(region); // for debugging
                 var thisArea = regionArea[region];
                 var thisFence = regionFence[region];
+                var orientedFence = thisFence.Select(f => regions.AreMerged(region, f.A) ? f : new Fence(f.B, f.A)).ToList();
                 // Find number of sides
                 UnionFind<Fence> sides = new();
-                for (int i = 0; i < thisFence.Count; i++)
+                for (int i = 0; i < orientedFence.Count; i++)
                 {
-                    for (int j = i + 1; j < thisFence.Count; j++)
+                    Fence fenceI = orientedFence[i];
+                    for (int j = i + 1; j < orientedFence.Count; j++)
                     {
                         // TODO something with thisFence[i] and thisFence[j]
-                        sides.Find(thisFence[i]);
-                        sides.Find(thisFence[j]);
+                        // sides.Find(thisFence[i]);
+                        // sides.Find(thisFence[j]);
+                                            
+                        Fence fenceJ = orientedFence[j];
+                        if (fenceI.AreAdjacentAligned(fenceJ))
+                        {
+                            sides.Union(fenceI, fenceJ);
+                        }
                     }
                 }
-                var sideCount = thisFence.Select(sides.Find).Distinct().Count();
+                var sideCount = orientedFence.Select(sides.Find).Distinct().Count();
                 var thisPrice = Math.BigMul(thisArea, sideCount);
                 total += thisPrice;
             }
 
             return total.ToString();
+        }
+
+        // Fence is defined by the two plots separated by the fence.
+        // Order matters. Let's say A is inside and B is outside.
+        public class Fence : IEquatable<Fence>
+        {
+            public VectorRC A { get; init; }
+            public VectorRC B { get; init; }
+
+            internal Fence(VectorRC X, VectorRC Y)
+            {
+                if (X == Y)
+                {
+                    throw new Exception("Must be distinct!");
+                }
+                if (Math.Abs(X.Row - Y.Row) >= 2 || Math.Abs(X.Col - Y.Col) >= 2)
+                {
+                    throw new Exception("Unaligned!");
+                }
+                if (Math.Abs(X.Row - Y.Row) == 1 && Math.Abs(X.Col - Y.Col) == 1)
+                {
+                    throw new Exception("Unaligned!");
+                }
+                Debug.Assert(Math.Abs(X.Row - Y.Row) == 0 || Math.Abs(X.Col - Y.Col) == 0);
+                if (Math.Abs(X.Row - Y.Row) == 0)
+                {
+                    Debug.Assert(Math.Abs(X.Col - Y.Col) == 1);
+                    A = X;
+                    B = Y;
+                    // if (X.Col < Y.Col)
+                    // {
+                    //     A = X;
+                    //     B = Y;
+                    // }
+                    // else
+                    // {
+                    //     A = Y;
+                    //     B = X;
+                    // }
+                }
+                else
+                {
+                    Debug.Assert(Math.Abs(X.Col - Y.Col) == 0);
+                    Debug.Assert(Math.Abs(X.Row - Y.Row) == 1);
+                    A = X;
+                    B = Y;
+                    // if (X.Row < Y.Row)
+                    // {
+                    //     A = X;
+                    //     B = Y;
+                    // }
+                    // else
+                    // {
+                    //     A = Y;
+                    //     B = X;
+                    // }
+                }
+            }
+
+            internal bool IsHorizontal => A.Col == B.Col;
+            internal bool IsVertical => A.Row == B.Row;
+
+            internal bool AreAdjacentAligned(Fence other)
+            {
+                if (this == other)
+                {
+                    return false;
+                }
+                if (this.IsHorizontal)
+                {
+                    if (!other.IsHorizontal)
+                    {
+                        return false;
+                    }
+                    return this.A.Row == other.A.Row && Math.Abs(this.A.Col - other.A.Col) == 1 && this.B.Row == other.B.Row;
+                }
+                else if (this.IsVertical)
+                {
+                    if (!other.IsVertical)
+                    {
+                        return false;
+                    }
+                    return this.A.Col == other.A.Col && Math.Abs(this.A.Row - other.A.Row) == 1 && this.B.Col == other.B.Col;
+                }
+                else
+                {
+                    throw new Exception("What orientation?");
+                }
+            }
+
+            // IEquatable
+            public bool Equals(Fence? other)
+            {
+                return ReferenceEquals(this, other) || (other is not null && this.A == other.A && this.B == other.B);
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return Equals(obj as Fence);
+            }
+
+            public override int GetHashCode()
+            {
+                var hasher = new HashCode();
+                hasher.Add(A);
+                hasher.Add(B);
+                return hasher.ToHashCode();
+            }
+
+            public override string ToString()
+            {
+                return $"Fence({A}|{B})";
+            }
+
+            public static bool operator ==(Fence? lhs, Fence? rhs)
+            {
+                return lhs?.Equals(rhs) ?? (lhs is null && rhs is null);
+            }
+            public static bool operator !=(Fence? lhs, Fence? rhs)
+            {
+                return !(lhs == rhs);
+            }
         }
     }
 }
--- Aoc2024Tests/Day12Tests.cs
+++ Aoc2024Tests/Day12Tests.cs
@@ -35,6 +35,27 @@
         }
 
         [TestMethod()]
+        public void Part2Example1Test()
+        {
+            var instance = new Day12(File.ReadAllText("day12-example1.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("80", answer);
+        }
+        [TestMethod()]
+        public void Part2Example2Test()
+        {
+            var instance = new Day12(File.ReadAllText("day12-example2.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("436", answer);
+        }
+        [TestMethod()]
+        public void Part2Example3Test()
+        {
+            var instance = new Day12(File.ReadAllText("day12-example3.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("1206", answer);
+        }
+        [TestMethod()]
         public void Part2Example4Test()
         {
             var instance = new Day12(File.ReadAllText("day12-example4.txt"));
@@ -47,13 +68,6 @@
             var instance = new Day12(File.ReadAllText("day12-example5.txt"));
             var answer = instance.Part2();
             Assert.AreEqual("368", answer);
-        }
-        [TestMethod()]
-        public void Part2Example3Test()
-        {
-            var instance = new Day12(File.ReadAllText("day12-example3.txt"));
-            var answer = instance.Part2();
-            Assert.AreEqual("1206", answer);
         }
         [TestMethod()]
         public void Part2InputTest()
