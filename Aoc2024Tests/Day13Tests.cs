9065f3c356b4759958e25a32f33a718d22ce2bef
Raymond Trinh <rtrinh3@gmail.com>
2024-12-13 18:51:26 -0500

    2024 day 13: implement Part2ExampleTest

--- Aoc2024Tests/Day13Tests.cs
+++ Aoc2024Tests/Day13Tests.cs
@@ -1,4 +1,6 @@
-﻿namespace Aoc2024.Tests
+﻿using System.Text.RegularExpressions;
+
+namespace Aoc2024.Tests
 {
     // https://adventofcode.com/2024/day/13
     // --- Day 13: Claw Contraption ---
@@ -20,6 +22,28 @@
             Assert.AreEqual("37128", answer);
         }
 
+        [TestMethod()]
+        public void Part2ExampleTest()
+        {
+            var example = File.ReadAllText("day13-example.txt");
+            var paragraphs = example.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+            Assert.AreEqual(paragraphs.Length, 4);
+            for (int i = 0; i < paragraphs.Length; i++)
+            {
+                var instance = new Day13(paragraphs[i]);
+                var answer = instance.Part2();
+                // Only the 2nd and the 4th are winnable
+                if (i == 1 || i == 3)
+                {
+                    // It'll take more than 100 tokens to win
+                    Assert.IsTrue(long.Parse(answer) > 100);
+                }
+                else
+                {
+                    Assert.IsTrue(long.Parse(answer) <= 0);
+                }
+            }
+        }
         [TestMethod()]
         public void Part2InputTest()
         {
