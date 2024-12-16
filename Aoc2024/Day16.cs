cd6df103b284becade6ec1fd29ce3cb81bec05df
Raymond Trinh <rtrinh3@gmail.com>
2024-12-16 08:02:41 -0500

    2024 day 16 part 1

---
+++ Aoc2024/Day16.cs
@@ -0,0 +1,38 @@
+﻿using AocCommon;
+
+namespace Aoc2024;
+
+// https://adventofcode.com/2024/day/16
+// --- Day 16: Reindeer Maze ---
+public class Day16(string input) : IAocDay
+{
+    private readonly Grid maze = new(input, '#');
+
+    private record Node(VectorRC Position, VectorRC Direction);
+
+    public string Part1()
+    {
+        VectorRC startPos = maze.Iterate().Single(x => x.Value == 'S').Position;
+        Node startNode = new(startPos, VectorRC.Right);
+        List<(Node, int)> GetNext(Node node)
+        {
+            var left = node with { Direction = node.Direction.RotatedLeft() };
+            var right = node with { Direction = node.Direction.RotatedRight() };
+            List<(Node, int)> next = [(left, 1000), (right, 1000)];
+            var forwardPosition = node.Position + node.Direction;
+            if (maze.Get(forwardPosition) != '#')
+            {
+                var forward = node with { Position = forwardPosition };
+                next.Add(new(forward, 1));
+            }
+            return next;
+        }
+        var result = GraphAlgos.DijkstraToEnd(startNode, GetNext, node => maze.Get(node.Position) == 'E');
+        return result.distance.ToString();
+    }
+
+    public string Part2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Aoc2024Tests/Day16Tests.cs
@@ -0,0 +1,44 @@
+﻿namespace Aoc2024.Tests;
+
+// https://adventofcode.com/2024/day/16
+// --- Day 16: Reindeer Maze ---
+[TestClass()]
+public class Day16Tests
+{
+    [TestMethod()]
+    public void Part1Example1Test()
+    {
+        var instance = new Day16(File.ReadAllText("day16-example1.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("7036", answer);
+    }
+    [TestMethod()]
+    public void Part1Example2Test()
+    {
+        var instance = new Day16(File.ReadAllText("day16-example2.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("11048", answer);
+    }
+    [TestMethod()]
+    public void Part1InputTest()
+    {
+        var instance = new Day16(File.ReadAllText("day16-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("133584", answer);
+    }
+
+    //[TestMethod()]
+    //public void Part2Example1Test()
+    //{
+    //    var instance = new Day16(File.ReadAllText("day16-example1.txt"));
+    //    var answer = instance.Part2();
+    //    Assert.AreEqual("9021", answer);
+    //}
+    //[TestMethod()]
+    //public void Part2InputTest()
+    //{
+    //    var instance = new Day16(File.ReadAllText("day16-input.txt"));
+    //    var answer = instance.Part2();
+    //    Assert.AreEqual("1535509", answer);
+    //}
+}
