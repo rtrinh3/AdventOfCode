f23c3d3ac8be4d3b9905094e6e3768f4a25ced79
Raymond Trinh <rtrinh3@gmail.com>
2024-12-19 08:33:40 -0500

    2024 day 19

---
+++ Aoc2024/Day19.cs
@@ -0,0 +1,98 @@
+﻿using AocCommon;
+
+namespace Aoc2024;
+
+public class Day19(string input) : IAocDay
+{
+    //private record class ArrangementNode(string Head, ArrangementNode? Tail);
+
+    public string Part1()
+    {
+        // Parse
+        string[] paragraphs = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+        string[] towels = paragraphs[0].Split(',', StringSplitOptions.TrimEntries).OrderByDescending(t => t.Length).ToArray();
+        string[] patterns = paragraphs[1].Split('\n');
+
+        // Find arrangements
+        //ArrangementNode?[] emptyArrangement = [null];
+        int answer = 0;
+        for (int i = 0; i < patterns.Length; i++)
+        {
+            var pattern = patterns[i];
+            Func<int, bool> CheckArrangements = _ => throw new Exception("Stub for memoized recursive function");
+            CheckArrangements = Memoization.Make((int index) =>
+            {
+                if (index >= pattern.Length)
+                {
+                    //return emptyArrangement;
+                    return true;
+                }
+                //List<ArrangementNode> possibleArrangements = new();
+                foreach (var towel in towels)
+                {
+                    if (pattern.AsSpan()[index..].StartsWith(towel))
+                    {
+                        //var tails = FindArrangements(index + towel.Length);
+                        //foreach (var tail in tails)
+                        //{
+                        //    ArrangementNode arrangement = new(towel, tail);
+                        //    possibleArrangements.Add(arrangement);
+                        //}
+                        bool tailPossible = CheckArrangements(index + towel.Length);
+                        if (tailPossible)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                //return possibleArrangements.ToArray();
+                return false;
+            });
+            var possible = CheckArrangements(0);
+            if (possible)
+            {
+                answer++;
+            }
+            //Console.WriteLine($"Done {i + 1}/{patterns.Length} -- answer={answer}");
+        }
+        return answer.ToString();
+    }
+
+    public string Part2()
+    {
+        // Parse
+        string[] paragraphs = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+        string[] towels = paragraphs[0].Split(',', StringSplitOptions.TrimEntries).OrderByDescending(t => t.Length).ToArray();
+        string[] patterns = paragraphs[1].Split('\n');
+
+        // Find arrangements
+        //ArrangementNode?[] emptyArrangement = [null];
+        long answer = 0;
+        for (int i = 0; i < patterns.Length; i++)
+        {
+            var pattern = patterns[i];
+            Func<int, long> FindArrangements = _ => throw new Exception("Stub for memoized recursive function");
+            FindArrangements = Memoization.Make((int index) =>
+            {
+                if (index >= pattern.Length)
+                {
+                    return 1;
+                }
+                long possibleArrangements = 0;
+                foreach (var towel in towels)
+                {
+                    if (pattern.AsSpan()[index..].StartsWith(towel))
+                    {
+                        var tails = FindArrangements(index + towel.Length);
+                        possibleArrangements += tails;
+                    }
+                }
+                return possibleArrangements;
+            });
+            var possible = FindArrangements(0);
+            answer += possible;
+            Console.WriteLine($"Done {i + 1}/{patterns.Length} -- answer={answer}");
+        }
+        return answer.ToString();
+    }
+}
---
+++ Aoc2024Tests/Day19Tests.cs
@@ -0,0 +1,34 @@
+﻿namespace Aoc2024.Tests;
+
+[TestClass()]
+public class Day19Tests
+{
+    [TestMethod()]
+    public void Part1ExampleTest()
+    {
+        var instance = new Day19(File.ReadAllText("day19-example.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("6", answer);
+    }
+    [TestMethod()]
+    public void Part1InputTest()
+    {
+        var instance = new Day19(File.ReadAllText("day19-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("365", answer);
+    }
+    [TestMethod()]
+    public void Part2ExampleTest()
+    {
+        var instance = new Day19(File.ReadAllText("day19-example.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("16", answer);
+    }
+    [TestMethod()]
+    public void Part2InputTest()
+    {
+        var instance = new Day19(File.ReadAllText("day19-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("730121486795169", answer);
+    }
+}
