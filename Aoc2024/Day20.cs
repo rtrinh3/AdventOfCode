382553a74c80f9e87bbdb73caf0f6a1932d5f7f9
Raymond Trinh <rtrinh3@gmail.com>
2024-12-20 08:48:11 -0500

    2024 day 20 part 1

---
+++ Aoc2024/Day20.cs
@@ -0,0 +1,68 @@
+﻿using AocCommon;
+
+namespace Aoc2024;
+
+// https://adventofcode.com/2024/day/20
+// --- Day 20: Race Condition ---
+public class Day20(string input) : IAocDay
+{
+    private readonly Grid map = new(input, '#');
+
+    public string Part1()
+    {
+        var cheats = FindCheats();
+        var answer = cheats.Count(c => c.Saved >= 100);
+        return answer.ToString();
+    }
+
+    public List<(VectorRC Start, VectorRC End, int Saved)> FindCheats()
+    {
+        VectorRC raceStart = map.Iterate().Single(x => x.Value == 'S').Position;
+        IEnumerable<VectorRC> originalNeighbors(VectorRC pos)
+        {
+            return pos.NextFour().Where(next => map.Get(next) != '#');
+        }
+        var originalRace = GraphAlgos.BfsToEnd(raceStart, originalNeighbors, pos => map.Get(pos) == 'E');
+        var originalTime = originalRace.distance;
+        List<VectorRC> cheatOffsets = new();
+        for (int i = 0; i <= 2; i++)
+        {
+            VectorRC offset = new(i, 2 - i);
+            cheatOffsets.Add(offset);
+            cheatOffsets.Add(-offset);
+            cheatOffsets.Add(offset.RotatedLeft());
+            cheatOffsets.Add(offset.RotatedRight());
+        }
+        var cheats = map.Iterate()
+            .Where(x => x.Value != '#')
+            .SelectMany(x => cheatOffsets.Select(offset => (Start: x.Position, End: x.Position + offset)))
+            .Distinct() // Why Distinct? Where are the duplicates coming from?
+            .Where(cheat => map.Get(cheat.End) != '#')
+            .Select(cheat =>
+            {
+                List<(VectorRC, int)> getNeighbors(VectorRC pos)
+                {
+                    List<(VectorRC, int)> results = pos.NextFour()
+                    .Where(next => map.Get(next) != '#')
+                    .Select(next => (next, 1))
+                    .ToList();
+                    if (pos == cheat.Start)
+                    {
+                        results.Add((cheat.End, 2));
+                    }
+                    return results;
+                }
+                var race = GraphAlgos.DijkstraToEnd(raceStart, getNeighbors, pos => map.Get(pos) == 'E');
+                var answer = (cheat.Start, cheat.End, originalTime - race.distance);
+                //Console.WriteLine(answer);
+                return answer;
+            })
+            .ToList();
+        return cheats;
+    }
+
+    public string Part2()
+    {
+        return nameof(Part2);
+    }
+}
---
+++ Aoc2024Tests/Day20Tests.cs
@@ -0,0 +1,40 @@
+﻿namespace Aoc2024.Tests;
+
+// https://adventofcode.com/2024/day/20
+// --- Day 20: Race Condition ---
+[TestClass()]
+public class Day20Tests
+{
+    [TestMethod()]
+    public void Part1ExampleTest()
+    {
+        var instance = new Day20(File.ReadAllText("day20-example.txt"));
+        Dictionary<int, int> expected = new()
+        {
+            [2] = 14,
+            [4] = 14,
+            [6] = 2,
+            [8] = 4,
+            [10] = 2,
+            [12] = 3,
+            [20] = 1,
+            [36] = 1,
+            [38] = 1,
+            [40] = 1,
+            [64] = 1
+        };
+        var cheats = instance.FindCheats();
+        var cheatGroups = cheats.GroupBy(c => c.Saved).ToDictionary(g => g.Key, g => g.Count());
+        foreach (var x in expected)
+        {
+            Assert.AreEqual(expected[x.Key], cheatGroups[x.Key]);
+        }
+    }
+    [TestMethod()]
+    public void Part1InputTest()
+    {
+        var instance = new Day20(File.ReadAllText("day20-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("1197", answer);
+    }
+}
