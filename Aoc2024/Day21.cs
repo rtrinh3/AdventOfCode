d947563a1c84246d8b5bc0e46ba4750fb91d42ad
Raymond Trinh <rtrinh3@gmail.com>
2024-12-21 08:18:50 -0500

    2024 day 21 stub

---
+++ Aoc2024/Day21.cs
@@ -0,0 +1,20 @@
+﻿using AocCommon;
+
+namespace Aoc2024;
+
+// https://adventofcode.com/2024/day/21
+// --- Day 21: Keypad Conundrum ---
+public class Day21(string input) : IAocDay
+{
+    private readonly string[] lines = input.TrimEnd().ReplaceLineEndings("\n").Split('\n');
+
+    public string Part1()
+    {
+        return nameof(Part1);
+    }
+
+    public string Part2()
+    {
+        return nameof(Part2);
+    }
+}
---
+++ Aoc2024Tests/Day21Tests.cs
@@ -0,0 +1,34 @@
+﻿namespace Aoc2024.Tests;
+
+// https://adventofcode.com/2024/day/21
+// --- Day 21: Keypad Conundrum ---
+[TestClass()]
+public class Day21Tests
+{
+    [TestMethod()]
+    public void Part1ExampleTest()
+    {
+        var file = File.ReadAllText("day21-example.txt");
+        var totalInstance = new Day21(file);
+        var totalAnswer = totalInstance.Part1();
+        Assert.AreEqual("126384", totalAnswer);
+
+        var lines = file.TrimEnd().ReplaceLineEndings("\n").Split('\n');
+        int[] expected = [68 * 29, 60 * 980, 68 * 179, 64 * 456, 64 * 379];
+        Assert.AreEqual(expected.Length, lines.Length);
+        for (int i = 0; i < expected.Length; i++)
+        {
+            var lineInstance = new Day21(lines[i]);
+            var lineAnswer = lineInstance.Part1();
+            Assert.AreEqual(expected[i].ToString(), lineAnswer);
+        }
+    }
+    [TestMethod()]
+    public void Part1InputTest()
+    {
+        var instance = new Day21(File.ReadAllText("day21-input.txt"));
+        var answer = instance.Part1();
+        //Assert.AreEqual("365", answer);
+        Assert.Inconclusive(answer);
+    }
+}
