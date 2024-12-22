5c9ad687d75917c6bc967322cbcbf7beadc6b8e3
Raymond Trinh <rtrinh3@gmail.com>
2024-12-22 08:18:45 -0500

    2024 day 22 part 1

---
+++ Aoc2024/Day22.cs
@@ -0,0 +1,32 @@
+﻿using AocCommon;
+
+namespace Aoc2024;
+
+// https://adventofcode.com/2024/day/22
+// --- Day 22: Monkey Market ---
+public class Day22(string input) : IAocDay
+{
+    private readonly long[] initialSecretNumbers = input.TrimEnd().ReplaceLineEndings("\n").Split('\n').Select(long.Parse).ToArray();
+
+    public string Part1()
+    {
+        long answer = 0;
+        foreach (var secret in initialSecretNumbers)
+        {
+            var number = secret;
+            for (int i = 0; i < 2000; i++)
+            {
+                number = ((number * 64) ^ number) % 16777216;
+                number = ((number / 32) ^ number) % 16777216;
+                number = ((number * 2048) ^ number) % 16777216;
+            }
+            answer += number;
+        }
+        return answer.ToString();
+    }
+
+    public string Part2()
+    {
+        return nameof(Part2);
+    }
+}
---
+++ Aoc2024Tests/Day22Tests.cs
@@ -0,0 +1,34 @@
+﻿namespace Aoc2024.Tests;
+
+// https://adventofcode.com/2024/day/22
+// --- Day 22: Monkey Market ---
+[TestClass()]
+public class Day22Tests
+{
+
+    [TestMethod()]
+    public void Part1ExampleTest()
+    {
+        var file = File.ReadAllText("day22-example.txt");
+        var totalInstance = new Day22(file);
+        var totalAnswer = totalInstance.Part1();
+        Assert.AreEqual("37327623", totalAnswer);
+
+        var lines = file.TrimEnd().ReplaceLineEndings("\n").Split('\n');
+        int[] expected = [8685429, 4700978, 15273692, 8667524];
+        Assert.AreEqual(expected.Length, lines.Length);
+        for (int i = 0; i < expected.Length; i++)
+        {
+            var lineInstance = new Day22(lines[i]);
+            var lineAnswer = lineInstance.Part1();
+            Assert.AreEqual(expected[i].ToString(), lineAnswer);
+        }
+    }
+    [TestMethod()]
+    public void Part1InputTest()
+    {
+        var instance = new Day22(File.ReadAllText("day22-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("13185239446", answer);
+    }
+}
