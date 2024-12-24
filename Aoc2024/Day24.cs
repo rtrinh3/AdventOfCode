121b101c8250b3218be0e96265f702ec067ebe49
Raymond Trinh <rtrinh3@gmail.com>
2024-12-24 08:16:59 -0500

    2024 day 24 part 1

---
+++ Aoc2024/Day24.cs
@@ -0,0 +1,63 @@
+﻿using AocCommon;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Aoc2024;
+
+// https://adventofcode.com/2024/day/24
+// --- Day 24: Crossed Wires ---
+public class Day24(string input) : IAocDay
+{
+    public string Part1()
+    {
+        var paragraphs = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+        Debug.Assert(paragraphs.Length == 2);
+        Dictionary<string, Lazy<bool>> wires = new();
+
+        var initials = paragraphs[0].Split('\n', StringSplitOptions.TrimEntries);
+        foreach (var line in initials)
+        {
+            var parts = line.Split(':', StringSplitOptions.TrimEntries);
+            Debug.Assert(parts.Length == 2);
+            string name = parts[0];
+            bool value = (parts[1] != "0");
+            wires[name] = new Lazy<bool>(() => value);
+        }
+
+        var gates = paragraphs[1].Split('\n', StringSplitOptions.TrimEntries);
+        foreach (var line in gates)
+        {
+            var parse = Regex.Match(line, @"^(\w+) (\w+) (\w+) -> (\w+)$");
+            Debug.Assert(parse.Success);
+            string lhs = parse.Groups[1].Value;
+            string op = parse.Groups[2].Value;
+            string rhs = parse.Groups[3].Value;
+            string target = parse.Groups[4].Value;
+            wires[target] = new Lazy<bool>(() =>
+            {
+                return op switch
+                {
+                    "AND" => wires[lhs].Value && wires[rhs].Value,
+                    "OR" => wires[lhs].Value || wires[rhs].Value,
+                    "XOR" => wires[lhs].Value ^ wires[rhs].Value,
+                    _ => throw new NotImplementedException(op)
+                };
+            });
+        }
+
+        ulong answer = 0;
+        foreach (var zWire in wires.Keys.Where(k => k.StartsWith('z')))
+        {
+            int position = int.Parse(zWire[1..]);
+            ulong value = wires[zWire].Value ? 1UL : 0UL;
+            answer |= (value << position);
+        }
+
+        return answer.ToString();
+    }
+
+    public string Part2()
+    {
+        return nameof(Day24);
+    }
+}
---
+++ Aoc2024Tests/Day24Tests.cs
@@ -0,0 +1,29 @@
+﻿namespace Aoc2024.Tests;
+
+// https://adventofcode.com/2024/day/24
+// --- Day 24: Crossed Wires ---
+[TestClass()]
+public class Day24Tests
+{
+    [TestMethod()]
+    public void Part1Example1Test()
+    {
+        var instance = new Day24(File.ReadAllText("day24-example1.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("4", answer);
+    }
+    [TestMethod()]
+    public void Part1Example2Test()
+    {
+        var instance = new Day24(File.ReadAllText("day24-example2.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("2024", answer);
+    }
+    [TestMethod()]
+    public void Part1InputTest()
+    {
+        var instance = new Day24(File.ReadAllText("day24-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("59364044286798", answer);
+    }
+}
