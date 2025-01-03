cc0f5a5ff9d7a23fcb12d9da44e393fba2b01ea6
Raymond Trinh <rtrinh3@gmail.com>
2025-01-03 10:42:59 -0500

    Implement Reddit challenge for 2024-11

--- Aoc2024/Day11.cs
+++ Aoc2024/Day11.cs
@@ -1,4 +1,5 @@
 ﻿using AocCommon;
+using System.Numerics;
 
 namespace Aoc2024
 {
@@ -26,9 +27,9 @@ namespace Aoc2024
             string digits = number.ToString();
             if (digits.Length % 2 == 0)
             {
-                var left = long.Parse(digits.Substring(0, digits.Length / 2));
+                var left = long.Parse(digits[..(digits.Length / 2)]);
                 var leftStones = SimulateStone(left, iterations - 1);
-                var right = long.Parse(digits.Substring(digits.Length / 2, digits.Length / 2));
+                var right = long.Parse(digits[(digits.Length / 2)..]);
                 var rightStones = SimulateStone(right, iterations - 1);
                 return memoSimulateStone[(number, iterations)] = leftStones + rightStones;
             }
@@ -59,6 +60,65 @@ namespace Aoc2024
         {
             long answer = DoPuzzle(75);
             return answer.ToString();
+        }
+
+        public string Part3(int iterations)
+        {
+            // https://www.reddit.com/r/adventofcode/comments/1hqoc5w/2024_day_11_i_made_a_part_3_to_this_day/
+            // https://breakmessage.com/aocextension/2024day11/
+            Func<EquatableArray<long>, int, (BigInteger checksum, BigInteger sumValues, BigInteger count)> Recurse = (_, _) => throw new Exception("Stub for memoized recursive function");
+            Recurse = Memoization.Make((EquatableArray<long> stones, int iterations) =>
+            {
+                if (iterations == 0)
+                {
+                    BigInteger checksum = 0;
+                    BigInteger sumValues = 0;
+                    BigInteger index = 0;
+                    foreach (var stone in stones)
+                    {
+                        checksum += index * stone;
+                        sumValues += stone;
+                        index++;
+                    }
+                    return (checksum, sumValues, index);
+                }
+                else
+                {
+                    BigInteger checksum = 0;
+                    BigInteger sumValues = 0;
+                    BigInteger index = 0;
+                    foreach (var stone in stones)
+                    {
+                        System.Collections.Immutable.ImmutableArray<long>? nextStones = null;
+                        if (stone == 0)
+                        {
+                            nextStones = [1];
+                        }
+                        else
+                        {
+                            string digits = stone.ToString();
+                            if (digits.Length % 2 == 0)
+                            {
+                                var left = long.Parse(digits[..(digits.Length / 2)]);
+                                var right = long.Parse(digits[(digits.Length / 2)..]);
+                                nextStones = [left, right];
+                            }
+                            else
+                            {
+                                nextStones = [stone * 2024];
+                            }
+                        }
+                        var next = Recurse(new(nextStones), iterations - 1);
+                        checksum += next.checksum + next.sumValues * index;
+                        sumValues += next.sumValues;
+                        index += next.count;
+                    }
+                    return (checksum, sumValues, index);
+                }
+            });
+
+            var answers = Recurse(new(initialStones), iterations);
+            return answers.checksum.ToString();
         }
     }
 }
--- Aoc2024Tests/Day11Tests.cs
+++ Aoc2024Tests/Day11Tests.cs
@@ -27,5 +27,25 @@
             var answer = instance.Part2();
             Assert.AreEqual("236302670835517", answer);
         }
+
+        [TestMethod()]
+        public void Part3ExampleTest()
+        {
+            // https://www.reddit.com/r/adventofcode/comments/1hqoc5w/2024_day_11_i_made_a_part_3_to_this_day/
+            // https://breakmessage.com/aocextension/2024day11/
+            var instance = new Day11(File.ReadAllText("day11-example.txt"));
+            (int iterations, string expected)[] tests =
+            {
+                (4, "72485"),
+                (6, "44893"),
+                (25, "2507617947379703"),
+                (75, "4490957455945801063263206957330247")
+            };
+            foreach (var test in tests)
+            {
+                var answer = instance.Part3(test.iterations);
+                Assert.AreEqual(test.expected, answer);
+            }
+        }
     }
 }
