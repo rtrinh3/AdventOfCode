efb5063d30b13260d5d11b25f64bb4b9cebd8c7a
Raymond Trinh <rtrinh3@gmail.com>
2025-01-11 11:07:21 -0500

    2024 day 24: shorten test timeout (I still don't have an automatic solution)

--- Aoc2024Tests/Day24Tests.cs
+++ Aoc2024Tests/Day24Tests.cs
@@ -51,12 +51,12 @@ public class Day24Tests
         var answer = instance.DoPart2(2, EvaluateErrorBitwiseAnd);        
         Assert.AreEqual("z00,z01,z02,z05", answer);
     }
-    [TestMethod(), Timeout(60_000)]
+    [TestMethod(), Timeout(10_000)]
     public void Part2InputTest()
     {
         var instance = new Day24(File.ReadAllText("day24-input.txt"));
         var answer = instance.Part2();
-        // (gmt,z07), (qjj,cbj), (dmn, z18), (cfk,z35)
+        // (gmt,z07);(cbj,qjj);(dmn,z18);(cfk,z35)
         Assert.AreEqual("cbj,cfk,dmn,gmt,qjj,z07,z18,z35", answer);
     }
 }
