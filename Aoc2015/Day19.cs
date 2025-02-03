632425dd462120b62e6e6a05b3622536117c7075
Raymond Trinh <rtrinh3@gmail.com>
2025-02-03 16:53:54 -0500

    2015: initial commit

---
+++ Aoc2015/Day19.cs
@@ -0,0 +1,19 @@
+﻿namespace Aoc2015;
+
+public class Day19 : AocCommon.IAocDay
+{
+    public Day19(string input)
+    {
+        // TODO
+    }
+
+    public string Part1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Aoc2015Tests/Day19Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace Aoc2015.Tests
+{
+    [TestClass()]
+    public class Day19Tests
+    {
+        [TestMethod()]
+        public void Part1Example1Test()
+        {
+            var instance = new Day19(File.ReadAllText("day19-example1.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("4", answer);
+        }
+        [TestMethod()]
+        public void Part1Example2Test()
+        {
+            var instance = new Day19(File.ReadAllText("day19-example2.txt"));
+            var answer = instance.Part1();
+            Assert.AreEqual("7", answer);
+        }
+        [TestMethod()]
+        public void Part1InputTest()
+        {
+            var instance = new Day19(File.ReadAllText("day19-input.txt"));
+            var answer = instance.Part1();
+            //Assert.AreEqual("6", answer);
+            Assert.Inconclusive(answer);
+        }
+
+        //[TestMethod()]
+        //public void Part2_Test()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
