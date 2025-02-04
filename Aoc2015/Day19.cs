26845ec1c5ef9c66a2315d33905742adc2f9a098
Raymond Trinh <rtrinh3@gmail.com>
2025-02-03 19:08:12 -0500

    2015 day 19 part 2

--- Aoc2015/Day19.cs
+++ Aoc2015/Day19.cs
@@ -5,32 +5,41 @@ using AocCommon;
 
 namespace Aoc2015;
 
-public class Day19(string input) : IAocDay
+// https://adventofcode.com/2015/day/19
+// --- Day 19: Medicine for Rudolph ---
+public class Day19 : IAocDay
 {
-    public string Part1()
+    private readonly (string InToken, string OutToken)[] rules;
+    private readonly string givenMolecule;
+
+    public Day19(string input)
     {
-        // Parse
         var paragraphs = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
         Debug.Assert(paragraphs.Length == 2);
-        var rules = paragraphs[0].Split('\n').Select(line =>
+        rules = paragraphs[0].Split('\n').Select(line =>
         {
             var parts = line.Split("=>", Parsing.TrimAndDiscard);
             Debug.Assert(parts.Length == 2);
             return (parts[0], parts[1]);
-        }).GroupBy(r => r.Item1)
-        .ToDictionary(g => g.Key, g => g.Select(r => r.Item2).ToArray());
-        string molecule = paragraphs[1];
+        }).ToArray();
+        givenMolecule = paragraphs[1];
+    }
 
+    public string Part1()
+    {
+        var rulesLookup = rules
+            .GroupBy(r => r.InToken)
+            .ToDictionary(g => g.Key, g => g.Select(r => r.OutToken).ToArray());
         // Replacements
-        string pattern = string.Join("|", rules.Keys);
-        var matches = Regex.EnumerateMatches(molecule, pattern);
+        string pattern = string.Join("|", rulesLookup.Keys);
+        var matches = Regex.EnumerateMatches(givenMolecule, pattern);
         HashSet<string> newMolecules = new();
         foreach (var match in matches)
         {
-            var prefix = molecule.AsSpan(0, match.Index);
-            var suffix = molecule.AsSpan(match.Index + match.Length);
-            var initial = molecule.Substring(match.Index, match.Length);
-            var replacements = rules[initial];
+            var prefix = givenMolecule.AsSpan(0, match.Index);
+            var suffix = givenMolecule.AsSpan(match.Index + match.Length);
+            var initial = givenMolecule.Substring(match.Index, match.Length);
+            var replacements = rulesLookup[initial];
             foreach (var replacement in replacements)
             {
                 StringBuilder replaced = new();
@@ -45,6 +54,42 @@ public class Day19(string input) : IAocDay
 
     public string Part2()
     {
-        throw new NotImplementedException();
+        // Work backwards from the given molecule towards the initial state, "e"
+        // Treat the "e => ..." rules separately because there are no "... => e" rules
+        // DFS, only works if the first solution has the best length
+        var initialRules = rules.Where(r => r.InToken == "e").ToArray();
+        var otherRules = rules.Where(r => r.InToken != "e").ToArray();
+        Stack<(string molecule, int replacements)> stack = new();
+        stack.Push((givenMolecule, 0));
+        while (stack.TryPop(out var state))
+        {
+            var (molecule, replacements) = state;
+            if (molecule == "e")
+            {
+                return replacements.ToString();
+            }
+            foreach (var r in initialRules)
+            {
+                if (molecule == r.OutToken)
+                {
+                    stack.Push(("e", replacements + 1)); // by definition of initialRules
+                }
+            }
+            for (int i = 0; i < molecule.Length; i++)
+            {
+                foreach (var r in otherRules)
+                {
+                    if (molecule.AsSpan(i).StartsWith(r.OutToken))
+                    {
+                        StringBuilder replaced = new();
+                        replaced.Append(molecule.AsSpan(0, i));
+                        replaced.Append(r.InToken);
+                        replaced.Append(molecule.AsSpan(i + r.OutToken.Length));
+                        stack.Push((replaced.ToString(), replacements + 1));
+                    }
+                }
+            }
+        }
+        throw new Exception("Answer not found");
     }
 }
--- Aoc2015Tests/Day19Tests.cs
+++ Aoc2015Tests/Day19Tests.cs
@@ -1,5 +1,7 @@
 ﻿namespace Aoc2015.Tests
 {
+    // https://adventofcode.com/2015/day/19
+    // --- Day 19: Medicine for Rudolph ---
     [TestClass()]
     public class Day19Tests
     {
@@ -25,10 +27,26 @@
             Assert.AreEqual("518", answer);
         }
 
-        //[TestMethod()]
-        //public void Part2_Test()
-        //{
-        //    Assert.Fail();
-        //}
+        [TestMethod()]
+        public void Part2Example1Test()
+        {
+            var instance = new Day19(File.ReadAllText("day19-example1.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("3", answer);
+        }
+        [TestMethod()]
+        public void Part2Example2Test()
+        {
+            var instance = new Day19(File.ReadAllText("day19-example2.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("6", answer);
+        }
+        [TestMethod()]
+        public void Part2InputTest()
+        {
+            var instance = new Day19(File.ReadAllText("day19-input.txt"));
+            var answer = instance.Part2();
+            Assert.AreEqual("200", answer);
+        }
     }
 }
