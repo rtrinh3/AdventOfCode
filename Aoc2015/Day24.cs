4a9b497827ad2fe3328b375ab7ed311b36fcec13
Raymond Trinh <rtrinh3@gmail.com>
2025-02-06 18:19:11 -0500

    2015 day 24

---
+++ Aoc2015/Day24.cs
@@ -0,0 +1,73 @@
+﻿using AocCommon;
+using System.Collections.Immutable;
+using System.Diagnostics;
+
+namespace Aoc2015;
+
+// https://adventofcode.com/2015/day/24
+// --- Day 24: It Hangs in the Balance ---
+public class Day24(string input) : IAocDay
+{
+    private readonly int[] weights = Parsing.IntsPositive(input);
+
+
+    private long DoPuzzle(int divisor)
+    {
+        int totalWeight = weights.Sum();
+        Debug.Assert(totalWeight % divisor == 0);
+        int targetWeight = totalWeight / divisor;
+
+        var rightSizedGroups = GenerateSubsets(weights, targetWeight).ToArray();
+        //foreach (var g in rightSizedGroups)
+        //{
+        //    Console.WriteLine(string.Join(" ", g));
+        //}
+
+        var minimumSize = rightSizedGroups.Min(g => g.Length);
+
+        var minimumGroups = rightSizedGroups.Where(g => g.Length == minimumSize);
+        var minimumQuantum = minimumGroups.Min(g => g.Aggregate(1L, (a, b) => a * b));
+
+        return minimumQuantum;
+    }
+
+    private static IEnumerable<int[]> GenerateSubsets(IEnumerable<int> items, int targetWeight)
+    {
+        Stack<(ImmutableHashSet<int> prefix, ImmutableHashSet<int> candidates)> stack = new();
+        stack.Push(([], items.ToImmutableHashSet()));
+        while (stack.TryPop(out var state))
+        {
+            var weight = state.prefix.Sum();
+            if (weight == targetWeight)
+            {
+                yield return state.prefix.ToArray();
+            }
+            else if (weight < targetWeight)
+            {
+                var subset = state.candidates;
+                foreach (var c in state.candidates)
+                {
+                    subset = subset.Remove(c);
+                    var newPrefix = state.prefix.Add(c);
+                    stack.Push((newPrefix, subset));
+                }
+            }
+            else
+            {
+                //"NOP".ToString();
+            }
+        }
+    }
+
+    public string Part1()
+    {
+        var answer = DoPuzzle(3);
+        return answer.ToString();
+    }
+
+    public string Part2()
+    {
+        var answer = DoPuzzle(4);
+        return answer.ToString();
+    }
+}
---
+++ Aoc2015Tests/Day24Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace Aoc2015.Tests;
+
+// https://adventofcode.com/2015/day/24
+// --- Day 24: It Hangs in the Balance ---
+[TestClass()]
+public class Day24Tests
+{
+    [TestMethod()]
+    public void Part1ExampleTest()
+    {
+        var instance = new Day24(File.ReadAllText("day24-example.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("99", answer);
+    }
+    [TestMethod(), Timeout(60_000)]
+    public void Part1InputTest()
+    {
+        var instance = new Day24(File.ReadAllText("day24-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("10439961859", answer);
+    }
+
+    [TestMethod()]
+    public void Part2ExampleTest()
+    {
+        var instance = new Day24(File.ReadAllText("day24-example.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("44", answer);
+    }
+    [TestMethod(), Timeout(60_000)]
+    public void Part2InputTest()
+    {
+        var instance = new Day24(File.ReadAllText("day24-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("72050269", answer);
+    }
+}
