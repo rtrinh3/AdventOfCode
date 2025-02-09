cfff0f2443c9b8ad2da319995deffa84047893ab
Raymond Trinh <rtrinh3@gmail.com>
2025-02-09 10:11:21 -0500

    2015 day 17

---
+++ Aoc2015/Day17.cs
@@ -0,0 +1,69 @@
+﻿using AocCommon;
+using System.Collections.Immutable;
+
+namespace Aoc2015;
+
+// https://adventofcode.com/2015/day/17
+// --- Day 17: No Such Thing as Too Much ---
+public class Day17(string input): IAocDay
+{
+    private readonly int[] containersInput = Parsing.IntsPositive(input);
+
+    private IEnumerable<ImmutableArray<int>> GenerateSubsets(int targetWeight)
+    {
+        Stack<(ImmutableArray<int> prefix, int sum, ImmutableStack<int> candidates)> stack = new();
+        var initialCandidates = ImmutableStack.CreateRange(containersInput);
+        stack.Push(([], 0, initialCandidates));
+        while (stack.TryPop(out var state))
+        {
+            var subset = state.candidates;
+            while (!subset.IsEmpty)
+            {
+                subset = subset.Pop(out var c);
+                var newPrefix = state.prefix.Add(c);
+                var newSum = state.sum + c;
+                if (newSum == targetWeight)
+                {
+                    yield return newPrefix;
+                }
+                else if (newSum < targetWeight)
+                {
+                    stack.Push((newPrefix, newSum, subset));
+                }
+            }
+        }
+    }
+
+    public string Part1()
+    {
+        var answer = DoPart1(150);
+        return answer.ToString();
+    }
+
+    public int DoPart1(int targetWeight)
+    {
+        var subsets = GenerateSubsets(targetWeight);
+        var answer = subsets.Count();
+        return answer;
+    }
+
+    public string Part2()
+    {
+        var answer = DoPart2(150);
+        return answer.ToString();
+    }
+
+    public int DoPart2(int targetWeight)
+    {
+        var subsets = GenerateSubsets(targetWeight);
+        var counts = new Dictionary<int, int>();
+        foreach (var containers in subsets)
+        {
+            int count = containers.Length;
+            counts[count] = 1 + counts.GetValueOrDefault(count, 0);
+        }
+        var minimumContainers = counts.Keys.Min();
+        var minimumCount = counts[minimumContainers];
+        return minimumCount;
+    }
+}
---
+++ Aoc2015Tests/Day17Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace Aoc2015.Tests;
+
+// https://adventofcode.com/2015/day/17
+// --- Day 17: No Such Thing as Too Much ---
+[TestClass()]
+public class Day17Tests
+{
+    [TestMethod()]
+    public void Part1ExampleTest()
+    {
+        var instance = new Day17(File.ReadAllText("day17-example.txt"));
+        var answer = instance.DoPart1(25);
+        Assert.AreEqual(4, answer);
+    }
+    [TestMethod()]
+    public void Part1InputTest()
+    {
+        var instance = new Day17(File.ReadAllText("day17-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("654", answer);
+    }
+
+    [TestMethod()]
+    public void Part2ExampleTest()
+    {
+        var instance = new Day17(File.ReadAllText("day17-example.txt"));
+        var answer = instance.DoPart2(25);
+        Assert.AreEqual(3, answer);
+    }
+    [TestMethod()]
+    public void Part2InputTest()
+    {
+        var instance = new Day17(File.ReadAllText("day17-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("57", answer);
+    }
+}
