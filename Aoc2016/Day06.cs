6fae07ecf26dd71b6d53eabdd45482c88567c8e2
Raymond Trinh <rtrinh3@gmail.com>
2025-02-10 21:10:56 -0500

    2016 day 6

---
+++ Aoc2016/Day06.cs
@@ -0,0 +1,55 @@
+﻿using AocCommon;
+using System.Diagnostics;
+
+namespace Aoc2016;
+
+// https://adventofcode.com/2016/day/6
+// --- Day 6: Signals and Noise ---
+public class Day06 : IAocDay
+{
+    private readonly string[] lines;
+    private readonly int length;
+
+    public Day06(string input)
+    {
+        lines = Parsing.SplitLines(input);
+        length = lines[0].Length;
+        Debug.Assert(lines.All(l => l.Length == length));
+    }
+
+    public string Part1()
+    {
+        char[] code = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            char mostFrequent = lines
+                .Select(l => l[i])
+                .GroupBy(c => c)
+                .Select(c => (c.Key, Count: c.Count()))
+                .OrderByDescending(c => c.Count)
+                .Select(c => c.Key)
+                .First();
+            code[i] = mostFrequent;
+        }
+        var answer = string.Join("", code);
+        return answer;
+    }
+
+    public string Part2()
+    {
+        char[] code = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            char mostFrequent = lines
+                .Select(l => l[i])
+                .GroupBy(c => c)
+                .Select(c => (c.Key, Count: c.Count()))
+                .OrderBy(c => c.Count)
+                .Select(c => c.Key)
+                .First();
+            code[i] = mostFrequent;
+        }
+        var answer = string.Join("", code);
+        return answer;
+    }
+}
---
+++ Aoc2016Tests/Day06Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace Aoc2016.Tests;
+
+// https://adventofcode.com/2016/day/6
+// --- Day 6: Signals and Noise ---
+[TestClass()]
+public class Day06Tests
+{
+    [TestMethod()]
+    public void Day06_Part1_Example_Test()
+    {
+        var instance = new Day06(File.ReadAllText("day06-example.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("easter", answer);
+    }
+    [TestMethod()]
+    public void Day06_Part1_Input_Test()
+    {
+        var instance = new Day06(File.ReadAllText("day06-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("qoclwvah", answer);
+    }
+
+    [TestMethod()]
+    public void Day06_Part2_Example_Test()
+    {
+        var instance = new Day06(File.ReadAllText("day06-example.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("advent", answer);
+    }
+    [TestMethod()]
+    public void Day06_Part2_Input_Test()
+    {
+        var instance = new Day06(File.ReadAllText("day06-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("ryrgviuv", answer);
+    }
+}
