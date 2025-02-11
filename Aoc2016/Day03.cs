70dd4056b8f44fa2ee457cccb31c58a4a9dcecb1
Raymond Trinh <rtrinh3@gmail.com>
2025-02-10 19:04:23 -0500

    2016 day 3

---
+++ Aoc2016/Day03.cs
@@ -0,0 +1,48 @@
+﻿using AocCommon;
+using System.Diagnostics;
+
+namespace Aoc2016;
+
+// https://adventofcode.com/2016/day/3
+// --- Day 3: Squares With Three Sides ---
+public class Day03(string input): IAocDay
+{
+    private readonly int[][] lines = Parsing.SplitLines(input).Select(Parsing.IntsPositive).ToArray();
+
+    public string Part1()
+    {
+        int triangles = 0;
+        foreach (var line in lines)
+        {
+            if (IsTriangle(line))
+            {
+                triangles++;
+            }
+        }
+        return triangles.ToString();
+    }
+
+    public string Part2()
+    {
+        Debug.Assert(lines.Length % 3 == 0);
+        var trios = lines.Select(x => x[0])
+            .Concat(lines.Select(x => x[1]))
+            .Concat(lines.Select(x => x[2]))
+            .Chunk(3);
+        int triangles = 0;
+        foreach (var trio in trios)
+        {
+            if (IsTriangle(trio))
+            {
+                triangles++;
+            }
+        }
+        return triangles.ToString();
+    }
+
+    private static bool IsTriangle(int[] trio)
+    {
+        Debug.Assert(trio.Length == 3);
+        return (trio[0] + trio[1] > trio[2]) && (trio[0] + trio[2] > trio[1]) && (trio[1] + trio[2] > trio[0]);
+    }
+}
---
+++ Aoc2016Tests/Day03Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace Aoc2016.Tests;
+
+// https://adventofcode.com/2016/day/3
+// --- Day 3: Squares With Three Sides ---
+[TestClass()]
+public class Day03Tests
+{
+    [TestMethod()]
+    public void Day03_Part1_Example_Test()
+    {
+        var instance = new Day03(File.ReadAllText("day03-example.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("0", answer);
+    }
+
+    [TestMethod()]
+    public void Day03_Part1_Input_Test()
+    {
+        var instance = new Day03(File.ReadAllText("day03-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("993", answer);
+    }
+
+    [TestMethod()]
+    public void Day03_Part2_Input_Test()
+    {
+        var instance = new Day03(File.ReadAllText("day03-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("1849", answer);
+    }
+}
