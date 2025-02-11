ad37ca47c87b81e3e4d6497dc1f3fb67f827b4f0
Raymond Trinh <rtrinh3@gmail.com>
2025-02-10 19:59:25 -0500

    2016 day 5

---
+++ Aoc2016/Day05.cs
@@ -0,0 +1,58 @@
+﻿using AocCommon;
+using System.Diagnostics;
+using System.Text;
+
+namespace Aoc2016;
+
+// https://adventofcode.com/2016/day/5
+// --- Day 5: How About a Nice Game of Chess? ---
+public class Day05(string input) : IAocDay
+{
+    private readonly string seed = input.Trim();
+    private readonly System.Security.Cryptography.MD5 hasher = System.Security.Cryptography.MD5.Create();
+
+    public string Part1()
+    {
+        StringBuilder answer = new StringBuilder();
+        int i = 0;
+        int found = 0;
+        while (found < 8)
+        {
+            var value = seed + i;
+            var hash = hasher.ComputeHash(Encoding.ASCII.GetBytes(value));
+            if (hash[0] == 0 && hash[1] == 0 && hash[2] <= 0x0F)
+            {
+                answer.AppendFormat("{0:x}", hash[2]);
+                found++;
+            }
+            i++;
+        }
+        return answer.ToString();
+    }
+
+    public string Part2()
+    {
+        char[] answer = new char[8];
+        int i = 0;
+        int found = 0;
+        while (found < 8)
+        {
+            var value = seed + i;
+            var hash = hasher.ComputeHash(Encoding.ASCII.GetBytes(value));
+            if (hash[0] == 0 && hash[1] == 0 && hash[2] <= 0x0F)
+            {
+                int position = hash[2] & 0x0f;
+                if (position < 8 && answer[position] == 0)
+                {
+                    int charValue = hash[3] >> 4;
+                    string character = charValue.ToString("x");
+                    Debug.Assert(character.Length == 1);
+                    answer[position] = character[0];
+                    found++;
+                }
+            }
+            i++;
+        }
+        return string.Join("", answer);
+    }
+}
---
+++ Aoc2016Tests/Day05Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace Aoc2016.Tests;
+
+// https://adventofcode.com/2016/day/5
+// --- Day 5: How About a Nice Game of Chess? ---
+[TestClass()]
+public class Day05Tests
+{
+    [TestMethod()]
+    public void Day05_Part1_Example_Test()
+    {
+        var instance = new Day05(File.ReadAllText("day05-example.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("18f47a30", answer);
+    }
+    [TestMethod()]
+    public void Day05_Part1_Input_Test()
+    {
+        var instance = new Day05(File.ReadAllText("day05-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("f97c354d", answer);
+    }
+
+    [TestMethod()]
+    public void Day05_Part2_Example_Test()
+    {
+        var instance = new Day05(File.ReadAllText("day05-example.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("05ace8e3", answer);
+    }
+    [TestMethod()]
+    public void Day05_Part2_Input_Test()
+    {
+        var instance = new Day05(File.ReadAllText("day05-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("863dde27", answer);
+    }
+}
