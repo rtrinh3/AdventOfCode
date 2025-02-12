1aa28cd2b12e792a41821cba932e8a8787f21644
Raymond Trinh <rtrinh3@gmail.com>
2025-02-12 08:17:20 -0500

    2016 day 9

---
+++ Aoc2016/Day09.cs
@@ -0,0 +1,52 @@
+﻿using AocCommon;
+using System.Text.RegularExpressions;
+
+namespace Aoc2016;
+
+public class Day09(string input) : IAocDay
+{
+    private readonly string cleanInput = Regex.Replace(input, @"\s", "");
+
+    public string Part1()
+    {
+        var answer = Decompress(cleanInput, false);
+        return answer.ToString();
+    }
+
+    public string Part2()
+    {
+        var answer = Decompress(cleanInput, true);
+        return answer.ToString();
+    }
+
+    private static long Decompress(string data, bool recurse)
+    {
+        var markers = Regex.Matches(data, @"\((\d+)x(\d+)\)");
+        long decompressedLength = 0;
+        int inputIndex = 0;
+        foreach (var marker in markers.Cast<Match>())
+        {
+            if (marker.Index < inputIndex)
+            {
+                continue;
+            }
+            decompressedLength += marker.Index - inputIndex;
+            int length = int.Parse(marker.Groups[1].ValueSpan);
+            int repeat = int.Parse(marker.Groups[2].ValueSpan);
+            long trueLength;
+            if (recurse)
+            {
+                var capture = data.Substring(marker.Index + marker.Length, length);
+                trueLength = Decompress(capture, true);
+            }
+            else
+            {
+                trueLength = length;
+            }
+            decompressedLength += trueLength * repeat;
+            inputIndex = marker.Index + marker.Length + length;
+        }
+        decompressedLength += data.Length - inputIndex;
+        return decompressedLength;
+    }
+}
