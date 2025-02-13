a9f8b9a4bddb78e5cdda5f5c250a7189f3bf145e
Raymond Trinh <rtrinh3@gmail.com>
2025-02-12 20:40:23 -0500

    2016 day 5: Multithreading

--- Aoc2016/Day05.cs
+++ Aoc2016/Day05.cs
@@ -1,5 +1,5 @@
 ﻿using AocCommon;
-using System.Diagnostics;
+using System.Collections.Concurrent;
 using System.Text;
 
 namespace Aoc2016;
@@ -8,51 +8,95 @@ namespace Aoc2016;
 // --- Day 5: How About a Nice Game of Chess? ---
 public class Day05(string input) : IAocDay
 {
+    private const int OUTPUT_LENGTH = 8;
+
+    private record Output(int Iteration, char Digit);
+
     private readonly string seed = input.Trim();
-    private readonly System.Security.Cryptography.MD5 hasher = System.Security.Cryptography.MD5.Create();
 
     public string Part1()
     {
-        StringBuilder answer = new StringBuilder();
-        int i = 0;
+        ConcurrentBag<Output> outputs = new();
+        int iterations = -1;
         int found = 0;
-        while (found < 8)
+        Thread[] consumers = new Thread[Environment.ProcessorCount];
+        for (int consumerIndex = 0; consumerIndex < consumers.Length; consumerIndex++)
         {
-            var value = seed + i;
-            var hash = hasher.ComputeHash(Encoding.ASCII.GetBytes(value));
-            if (hash[0] == 0 && hash[1] == 0 && hash[2] <= 0x0F)
+            Thread consumer = new Thread(() =>
             {
-                answer.AppendFormat("{0:x}", hash[2]);
-                found++;
-            }
-            i++;
+                var hasher = System.Security.Cryptography.MD5.Create();
+                while (found < OUTPUT_LENGTH)
+                {
+                    int i = Interlocked.Increment(ref iterations);
+                    var hashInput = seed + i;
+                    var hash = hasher.ComputeHash(Encoding.ASCII.GetBytes(hashInput));
+                    if (hash[0] == 0 && hash[1] == 0 && hash[2] <= 0x0F)
+                    {
+                        string digit = hash[2].ToString("x");
+                        outputs.Add(new(i, digit[0]));
+                        Interlocked.Increment(ref found);
+                    }
+                }
+            });
+            consumers[consumerIndex] = consumer;
+            consumer.Start();
         }
-        return answer.ToString();
+        foreach (var consumer in consumers)
+        {
+            consumer.Join();
+        }
+        //Console.WriteLine(iterations + " iterations");
+        var wantedOutputs = outputs.OrderBy(x => x.Iteration).Select(x => x.Digit).Take(OUTPUT_LENGTH);
+        string answer = string.Join("", wantedOutputs);
+        return answer;
     }
 
     public string Part2()
     {
-        char[] answer = new char[8];
-        int i = 0;
-        int found = 0;
-        while (found < 8)
+        ConcurrentBag<Output>[] outputs = new ConcurrentBag<Output>[OUTPUT_LENGTH];
+        for (int i = 0; i < outputs.Length; i++)
         {
-            var value = seed + i;
-            var hash = hasher.ComputeHash(Encoding.ASCII.GetBytes(value));
-            if (hash[0] == 0 && hash[1] == 0 && hash[2] <= 0x0F)
-            {
-                int position = hash[2] & 0x0f;
-                if (position < 8 && answer[position] == 0)
-                {
-                    int charValue = hash[3] >> 4;
-                    string character = charValue.ToString("x");
-                    Debug.Assert(character.Length == 1);
-                    answer[position] = character[0];
-                    found++;
-                }
-            }
-            i++;
+            outputs[i] = new();
         }
-        return string.Join("", answer);
+        int iterations = -1;
+        int found = 0;
+        Thread[] consumers = new Thread[Environment.ProcessorCount];
+        for (int consumerIndex = 0; consumerIndex < consumers.Length; consumerIndex++)
+        {
+            Thread consumer = new Thread(() =>
+            {
+                var hasher = System.Security.Cryptography.MD5.Create();
+                while (found < OUTPUT_LENGTH)
+                {
+                    int i = Interlocked.Increment(ref iterations);
+                    var hashInput = seed + i;
+                    var hash = hasher.ComputeHash(Encoding.ASCII.GetBytes(hashInput));
+                    if (hash[0] == 0 && hash[1] == 0 && hash[2] <= 0x0F)
+                    {
+                        int position = hash[2] & 0x0f;
+                        if (position < OUTPUT_LENGTH)
+                        {
+                            int charValue = hash[3] >> 4;
+                            string digit = charValue.ToString("x");
+                            if (outputs[position].IsEmpty)
+                            {
+                                Interlocked.Increment(ref found);
+                            }
+                            outputs[position].Add(new(i, digit[0]));
+                        }
+                    }
+                }
+            });
+            consumers[consumerIndex] = consumer;
+            consumer.Start();
+        }
+        foreach (var consumer in consumers)
+        {
+            consumer.Join();
+        }
+        //Console.WriteLine(iterations + " iterations");
+        var wantedOutputs = outputs.Select(bag => bag.MinBy(x => x.Iteration).Digit);
+        string answer = string.Join("", wantedOutputs);
+        return answer;
     }
 }
