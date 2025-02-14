f9d6e65473831e12e2e156fb7438e359143e26d6
Raymond Trinh <rtrinh3@gmail.com>
2025-02-14 16:42:49 -0500

    2016 day 14

---
+++ Aoc2016/Day14.cs
@@ -0,0 +1,98 @@
+﻿using AocCommon;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc2016;
+
+// https://adventofcode.com/2016/day/14
+// --- Day 14: One-Time Pad ---
+public class Day14(string input) : IAocDay
+{
+    private Regex trioMatcher = new(@"(.)\1\1");
+
+    private static string HashString(string hashInputString)
+    {
+        var hashInputBytes = Encoding.ASCII.GetBytes(hashInputString);
+        var hashBytes = MD5.HashData(hashInputBytes);
+        var hashString = string.Join("", hashBytes.Select(b => b.ToString("x2")));
+        return hashString;
+    }
+
+    private readonly Func<int, string> CalculateHashPart1 = Memoization.MakeInt((int suffix) =>
+    {
+        string hashInputString = input + suffix;
+        return HashString(hashInputString);
+    });
+
+    public string Part1()
+    {
+        int i = 0;
+        int found = 0;
+        while (true)
+        {
+            var hashI = CalculateHashPart1(i);
+            var trioMatch = trioMatcher.Match(hashI);
+            if (trioMatch.Success)
+            {
+                string five = trioMatch.Value + trioMatch.ValueSpan[0] + trioMatch.ValueSpan[0];
+                for (int j = 1; j <= 1000; j++)
+                {
+                    var hashJ = CalculateHashPart1(i + j);
+                    if (hashJ.Contains(five))
+                    {
+                        found++;
+                        if (found == 64)
+                        {
+                            var answer = i;
+                            return answer.ToString();
+                        }
+                        break;
+                    }
+                }
+            }
+            i++;
+        }
+        throw new Exception("Answer not found");
+    }
+    private readonly Func<int, string> CalculateHashPart2 = Memoization.MakeInt((int suffix) =>
+    {
+        string hashString = input + suffix;
+        for (int i = 0; i < 2017; i++)
+        {
+            hashString = HashString(hashString);
+        }
+        return hashString;
+    });
+
+    public string Part2()
+    {
+        int i = 0;
+        int found = 0;
+        while (true)
+        {
+            var hashI = CalculateHashPart2(i);
+            var trioMatch = trioMatcher.Match(hashI);
+            if (trioMatch.Success)
+            {
+                string five = trioMatch.Value + trioMatch.ValueSpan[0] + trioMatch.ValueSpan[0];
+                for (int j = 1; j <= 1000; j++)
+                {
+                    var hashJ = CalculateHashPart2(i + j);
+                    if (hashJ.Contains(five))
+                    {
+                        found++;
+                        if (found == 64)
+                        {
+                            var answer = i;
+                            return answer.ToString();
+                        }
+                        break;
+                    }
+                }
+            }
+            i++;
+        }
+        throw new Exception("Answer not found");
+    }
+}
---
+++ Aoc2016Tests/Day14Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace Aoc2016.Tests;
+
+// https://adventofcode.com/2016/day/14
+// --- Day 14: One-Time Pad ---
+[TestClass()]
+public class Day14Tests
+{
+    [TestMethod()]
+    public void Day14_Part1_Example_Test()
+    {
+        var instance = new Day14(File.ReadAllText("day14-example.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("22728", answer);
+    }
+    [TestMethod()]
+    public void Day14_Part1_Input_Test()
+    {
+        var instance = new Day14(File.ReadAllText("day14-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("18626", answer);
+    }
+
+    [TestMethod()]
+    public void Day14_Part2_Example_Test()
+    {
+        var instance = new Day14(File.ReadAllText("day14-example.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("22551", answer);
+    }
+    [TestMethod()]
+    public void Day14_Part2_Input_Test()
+    {
+        var instance = new Day14(File.ReadAllText("day14-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("20092", answer);
+    }
+}
