e7e564cf1eba354b0c8dbb58cbe6137a0d6f88e3
Raymond Trinh <rtrinh3@gmail.com>
2025-02-15 09:46:54 -0500

    2016 day 18

---
+++ Aoc2016/Day18.cs
@@ -0,0 +1,42 @@
+﻿namespace Aoc2016;
+
+// https://adventofcode.com/2016/day/18
+// --- Day 18: Like a Rogue ---
+public class Day18(string input) : AocCommon.IAocDay
+{
+    public int DoPuzzle(int rows)
+    {
+        bool[] row = input.Trim().Select(c => c == '^').ToArray();
+        int answer = row.Count(t => !t);
+        for (int i = 1; i < rows; i++)
+        {
+            var newRow = new bool[row.Length];
+            for (int j = 0; j < newRow.Length; j++)
+            {
+                bool left = row.ElementAtOrDefault(j - 1);
+                bool center = row[j];
+                bool right = row.ElementAtOrDefault(j + 1);
+                bool newTile = (left && center && !right) || (center && right && !left) || (left && !center && !right) || (right && !left && !center);
+                newRow[j] = newTile;
+                if (!newTile)
+                {
+                    answer++;
+                }
+            }
+            row = newRow;
+        }
+        return answer;
+    }
+
+    public string Part1()
+    {
+        var answer = DoPuzzle(40);
+        return answer.ToString();
+    }
+
+    public string Part2()
+    {
+        var answer = DoPuzzle(400000);
+        return answer.ToString();
+    }
+}
---
+++ Aoc2016Tests/Day18Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace Aoc2016.Tests;
+
+// https://adventofcode.com/2016/day/18
+// --- Day 18: Like a Rogue ---
+[TestClass()]
+public class Day18Tests
+{
+    [TestMethod()]
+    public void Day18_Part1_Example_Test()
+    {
+        var instance = new Day18(File.ReadAllText("day18-example.txt"));
+        var answer = instance.DoPuzzle(10);
+        Assert.AreEqual(38, answer);
+    }
+    [TestMethod()]
+    public void Day18_Part1_Input_Test()
+    {
+        var instance = new Day18(File.ReadAllText("day18-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("2013", answer);
+    }
+
+    [TestMethod()]
+    public void Day18_Part2_Input_Test()
+    {
+        var instance = new Day18(File.ReadAllText("day18-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("20006289", answer);
+    }
+}
