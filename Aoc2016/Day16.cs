f766fb106bf5b799525c07e510583e53f442d99c
Raymond Trinh <rtrinh3@gmail.com>
2025-02-15 08:50:27 -0500

    2016 day 16

---
+++ Aoc2016/Day16.cs
@@ -0,0 +1,35 @@
+﻿using AocCommon;
+
+namespace Aoc2016;
+
+// https://adventofcode.com/2016/day/16
+// --- Day 16: Dragon Checksum ---
+public class Day16(string input) : IAocDay
+{
+    public string Part1()
+    {
+        return DoPuzzle(272);
+    }
+
+    public string DoPuzzle(int diskSize)
+    {
+        bool[] a = input.Trim().Select(x => x == '1').ToArray();
+        while (a.Length < diskSize)
+        {
+            var b = a.Reverse().Select(x => !x);
+            a = [.. a, false, .. b];
+        }
+        bool[] checksum = a[..diskSize];
+        while (checksum.Length % 2 == 0)
+        {
+            checksum = checksum.Chunk(2).Select(pair => pair[0] == pair[1]).ToArray();
+        }
+        var answer = string.Join("", checksum.Select(x => x ? '1' : '0'));
+        return answer;
+    }
+
+    public string Part2()
+    {
+        return DoPuzzle(35651584);
+    }
+}
---
+++ Aoc2016Tests/Day16Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace Aoc2016.Tests;
+
+// https://adventofcode.com/2016/day/16
+// --- Day 16: Dragon Checksum ---
+[TestClass()]
+public class Day16Tests
+{
+    [TestMethod()]
+    public void Day16_Part1_Example_Test()
+    {
+        var instance = new Day16(File.ReadAllText("day16-example.txt"));
+        var answer = instance.DoPuzzle(20);
+        Assert.AreEqual("01100", answer);
+    }
+    [TestMethod()]
+    public void Day16_Part1_Input_Test()
+    {
+        var instance = new Day16(File.ReadAllText("day16-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("10111110010110110", answer);
+    }
+
+    [TestMethod()]
+    public void Day16_Part2_Input_Test()
+    {
+        var instance = new Day16(File.ReadAllText("day16-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("01101100001100100", answer);
+    }
+}
