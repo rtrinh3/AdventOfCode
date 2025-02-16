5896ea0944e1d438098c5f7d56a1e8f2b56b2ef2
Raymond Trinh <rtrinh3@gmail.com>
2025-02-16 08:44:42 -0500

    2016 day 21

---
+++ Aoc2016/Day21.cs
@@ -0,0 +1,131 @@
+﻿using AocCommon;
+using System.Diagnostics;
+
+namespace Aoc2016;
+
+// https://adventofcode.com/2016/day/21
+// --- Day 21: Scrambled Letters and Hash ---
+public class Day21(string input) : IAocDay
+{
+    public string Scramble(string password)
+    {
+        char[] buffer = password.Trim().ToCharArray();
+        char[] temp = new char[buffer.Length];
+        var lines = Parsing.SplitLines(input);
+        foreach (var line in lines)
+        {
+            if (line.StartsWith("swap position"))
+            {
+                var args = Parsing.IntsPositive(line);
+                Debug.Assert(args.Length == 2);
+                (buffer[args[0]], buffer[args[1]]) = (buffer[args[1]], buffer[args[0]]);
+            }
+            else if (line.StartsWith("swap letter"))
+            {
+                char letterA = line[12];
+                char letterB = line[26];
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    if (buffer[i] == letterA)
+                    {
+                        buffer[i] = letterB;
+                    }
+                    else if (buffer[i] == letterB)
+                    {
+                        buffer[i] = letterA;
+                    }
+                }
+            }
+            else if (line.StartsWith("rotate left"))
+            {
+                var args = Parsing.IntsPositive(line);
+                Debug.Assert(args.Length == 1);
+                int dist = args[0];
+                buffer.CopyTo(temp, 0);
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    buffer[i] = temp[(i + dist) % temp.Length];
+                }
+            }
+            else if (line.StartsWith("rotate right"))
+            {
+                var args = Parsing.IntsPositive(line);
+                Debug.Assert(args.Length == 1);
+                int dist = args[0];
+                int offset = -dist;
+                while (offset < 0)
+                {
+                    offset += temp.Length;
+                }
+                buffer.CopyTo(temp, 0);
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    buffer[i] = temp[(i + offset) % temp.Length];
+                }
+            }
+            else if (line.StartsWith("rotate based on position of letter"))
+            {
+                char letter = line[35];
+                int indexOfLetter = Array.IndexOf(buffer, letter);
+                int dist = 1 + indexOfLetter + (indexOfLetter >= 4 ? 1 : 0);
+                int offset = -dist;
+                while (offset < 0)
+                {
+                    offset += temp.Length;
+                }
+                buffer.CopyTo(temp, 0);
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    buffer[i] = temp[(i + offset) % temp.Length];
+                }
+            }
+            else if (line.StartsWith("reverse positions"))
+            {
+                var args = Parsing.IntsPositive(line);
+                Array.Reverse(buffer, args[0], args[1] - args[0] + 1);
+            }
+            else if (line.StartsWith("move position"))
+            {
+                var args = Parsing.IntsPositive(line);
+                Debug.Assert(args.Length == 2);
+                int src = args[0];
+                int dst = args[1];
+                int copyLength = Math.Abs(dst - src);
+                buffer.CopyTo(temp, 0);
+                if (src < dst)
+                {
+                    Array.Copy(temp, src + 1, buffer, src, copyLength);
+                }
+                else if (dst < src)
+                {
+                    Array.Copy(temp, dst, buffer, dst + 1, copyLength);
+                }
+                buffer[dst] = temp[src];
+            }
+        }
+        string answer = new(buffer);
+        return answer;
+    }
+
+    public string Part1()
+    {
+        var answer = Scramble("abcdefgh");
+        return answer;
+    }
+
+    public string Part2()
+    {
+        string scrambled = "fbgdceah";
+        var permutations = MoreMath.IteratePermutations(scrambled);
+        foreach (var potentialSource in permutations)
+        {
+            string sourceString = new(potentialSource);
+            var scramblerOutput = Scramble(sourceString);
+            if (scramblerOutput == scrambled)
+            {
+                return sourceString;
+            }
+        }
+        throw new Exception("No answer found");
+    }
+}
---
+++ Aoc2016Tests/Day21Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace Aoc2016.Tests;
+
+// https://adventofcode.com/2016/day/21
+// --- Day 21: Scrambled Letters and Hash ---
+[TestClass()]
+public class Day21Tests
+{
+    [TestMethod()]
+    public void Day21_Part1_Example_Test()
+    {
+        var instance = new Day21(File.ReadAllText("day21-example.txt"));
+        var answer = instance.Scramble("abcde");
+        Assert.AreEqual("decab", answer);
+    }
+    [TestMethod()]
+    public void Day21_Part1_Input_Test()
+    {
+        var instance = new Day21(File.ReadAllText("day21-input.txt"));
+        var answer = instance.Part1();
+        Assert.AreEqual("hcdefbag", answer);
+    }
+
+    [TestMethod()]
+    public void Day21_Part2_Input_Test()
+    {
+        var instance = new Day21(File.ReadAllText("day21-input.txt"));
+        var answer = instance.Part2();
+        Assert.AreEqual("fbhaegdc", answer);
+    }
+}
