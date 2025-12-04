be68c9c12b296def050d20ac063c99b770e1d436
Raymond Trinh <rtrinh3@gmail.com>
2025-12-04 08:08:39 -0500

    2025 day 4 part 2

--- Aoc2025/Day04.cs
+++ Aoc2025/Day04.cs
@@ -6,16 +6,16 @@ namespace Aoc2025;
 // --- Day 4: Printing Department ---
 public class Day04(string input) : IAocDay
 {
-    private readonly Grid grid = new(input, '.');
+    private readonly Grid initialGrid = new(input, '.');
 
     public string Part1()
     {
         int accumulator = 0;
-        foreach (var cell in grid.Iterate())
+        foreach (var cell in initialGrid.Iterate())
         {
             if (cell.Value == '@')
             {
-                int adjacentRolls = cell.Position.NextEight().Count(p => grid.Get(p) == '@');
+                int adjacentRolls = cell.Position.NextEight().Count(p => initialGrid.Get(p) == '@');
                 if (adjacentRolls < 4)
                 {
                     accumulator++;
@@ -27,6 +27,48 @@ public class Day04(string input) : IAocDay
 
     public string Part2()
     {
-        throw new NotImplementedException();
+        bool[,] grid = new bool[initialGrid.Height, initialGrid.Width];
+        foreach (var cell in initialGrid.Iterate())
+        {
+            if (cell.Value == '@')
+            {
+                grid[cell.Position.Row, cell.Position.Col] = true;
+            }
+        }
+
+        int accumulator = 0;
+        bool removed = false;
+        do
+        {
+            removed = false;
+            for (int r = 0; r < initialGrid.Height; r++)
+            {
+                for (int c = 0; c < initialGrid.Width; c++)
+                {
+                    if (grid[r, c])
+                    {
+                        int adjacentRolls = 0;
+                        foreach (var pos in new VectorRC(r, c).NextEight())
+                        {
+                            if (pos.Row >= 0 && pos.Row < initialGrid.Height && pos.Col >= 0 && pos.Col < initialGrid.Width)
+                            {
+                                if (grid[pos.Row, pos.Col])
+                                {
+                                    adjacentRolls++;
+                                }
+                            }
+                        }
+                        if (adjacentRolls < 4)
+                        {
+                            grid[r, c] = false;
+                            removed = true;
+                            accumulator++;
+                        }
+                    }
+                }
+            }
+        }
+        while (removed);
+        return accumulator.ToString();
     }
 }
--- Aoc2025Tests/Day04Tests.cs
+++ Aoc2025Tests/Day04Tests.cs
@@ -1,5 +1,7 @@
 namespace Aoc2025.Tests;
 
+// https://adventofcode.com/2025/day/4
+// --- Day 4: Printing Department ---
 [TestClass()]
 public class Day04Tests
 {
@@ -24,8 +26,7 @@ public class Day04Tests
     {
         var instance = new Day04(File.ReadAllText("day04-example.txt"));
         var answer = instance.Part2();
-        Assert.Inconclusive(answer);
-        // Assert.AreEqual("EXPECTED_ANSWER", answer);
+        Assert.AreEqual("43", answer);
     }
 
     [TestMethod()]
@@ -33,7 +34,6 @@ public class Day04Tests
     {
         var instance = new Day04(File.ReadAllText("day04-input.txt"));
         var answer = instance.Part2();
-        Assert.Inconclusive(answer);
-        // Assert.AreEqual("EXPECTED_ANSWER", answer);
+        Assert.AreEqual("9050", answer);
     }
 }
