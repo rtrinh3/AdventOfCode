80b4bd1526e6285a5d5ec53206bbbe3b185930ca
Raymond Trinh <rtrinh3@gmail.com>
2025-12-07 08:09:45 -0500

    2025 day 7: cleanup

--- Aoc2025/Day07.cs
+++ Aoc2025/Day07.cs
@@ -6,50 +6,35 @@ public class Day07(string input) : AocCommon.IAocDay
 {
     public string Part1()
     {
-        string[] rows = input.ReplaceLineEndings("\n").Split('\n', StringSplitOptions.RemoveEmptyEntries);
-        HashSet<int> beams = new();
-        int splitCount = 0;
-        foreach (string row in rows)
-        {
-            HashSet<int> newBeams = new();
-            for (int i = 0; i < row.Length; i++)
-            {
-                if (row[i] == 'S')
-                {
-                    newBeams.Add(i);
-                }
-            }
-            foreach (var beam in beams)
-            {
-                if (row[beam] == '^')
-                {
-                    newBeams.Add(beam - 1);
-                    newBeams.Add(beam + 1);
-                    splitCount++;
-                }
-                else
-                {
-                    newBeams.Add(beam);
-                }
-            }
-            beams = newBeams;
-        }
-        return splitCount.ToString();
+        var (Part1Answer, _) = DoPuzzle();
+        return Part1Answer.ToString();
     }
 
     public string Part2()
     {
+        var (_, Part2Answer) = DoPuzzle();
+        return Part2Answer.ToString();
+    }
+
+    private (int Part1Answer, long Part2Answer) DoPuzzle()
+    {
         string[] rows = input.ReplaceLineEndings("\n").Split('\n', StringSplitOptions.RemoveEmptyEntries);
         AocCommon.DefaultDict<int, long> beams = new();
+        int splitCount = 0;
+        bool firstRow = true;
         foreach (string row in rows)
         {
             AocCommon.DefaultDict<int, long> newBeams = new();
-            for (int i = 0; i < row.Length; i++)
+            if (firstRow)
             {
-                if (row[i] == 'S')
+                for (int col = 0; col < row.Length; col++)
                 {
-                    newBeams[i]++;
+                    if (row[col] == 'S')
+                    {
+                        newBeams[col]++;
+                    }
                 }
+                firstRow = false;
             }
             foreach (var (beam, count) in beams)
             {
@@ -57,6 +42,7 @@ public class Day07(string input) : AocCommon.IAocDay
                 {
                     newBeams[beam - 1] += count;
                     newBeams[beam + 1] += count;
+                    splitCount++;
                 }
                 else
                 {
@@ -65,7 +51,6 @@ public class Day07(string input) : AocCommon.IAocDay
             }
             beams = newBeams;
         }
-        var answer = beams.Values.Sum();
-        return answer.ToString();
+        return (splitCount, beams.Values.Sum());
     }
 }
